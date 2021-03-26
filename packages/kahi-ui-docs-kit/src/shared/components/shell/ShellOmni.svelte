44313c754bcb2c27e780d0ab701f094c51231bcd
novacbn <31122674+novacbn@users.noreply.github.com>
2021-03-26 07:10:23 -0400

    - [DocsKit] Updated `ShellOmni` to use modal for overflow on smaller viewports instead of Popovers

--- packages/kahi-ui-docs-kit/src/shared/components/shell/ShellOmni.svelte
+++ packages/kahi-ui-docs-kit/src/shared/components/shell/ShellOmni.svelte
@@ -1,10 +1,50 @@
 <script>
-    import {Anchor, Button, Menu, Omni, Popover} from "@kahi-ui/svelte";
+    import {Anchor, Button, Dialog, Menu, Omni, Spacer} from "@kahi-ui/svelte";
+
+    import {Menu as MenuIcon} from "svelte-feather/components/Menu";
+    import {X} from "svelte-feather/components/X";
 
     export let branding = "N/A";
     export let center = [];
     export let right = [];
 </script>
+
+<Dialog.Container id="shell-omni-navigation" palette="light" viewport="small" hidden="medium large">
+    <Dialog.Region>
+        <Dialog.Heading class="shell-omni-heading">
+            <span />
+            <Dialog.Button palette="dark" variation="clear" size="small">
+                <X size="1.25em" />
+                <Spacer orientation="horizontal" spacing="tiny" />
+                Close
+            </Dialog.Button>
+        </Dialog.Heading>
+
+        <Dialog.Body class="shell-omni-body">
+            <Menu.Container palette="light">
+                {#each center as link (link.href)}
+                    <Menu.Anchor
+                        href={link.href}
+                        target={link.is_external ? "_blank" : undefined}
+                        rel={link.is_external ? "noopener noreferrer" : undefined}
+                        >{link.text}</Menu.Anchor
+                    >
+                {/each}
+
+                <Menu.Divider />
+
+                {#each right as link (link.href)}
+                    <Menu.Anchor
+                        href={link.href}
+                        target={link.is_external ? "_blank" : undefined}
+                        rel={link.is_external ? "noopener noreferrer" : undefined}
+                        >{link.text}</Menu.Anchor
+                    >
+                {/each}
+            </Menu.Container>
+        </Dialog.Body>
+    </Dialog.Region>
+</Dialog.Container>
 
 <Omni.Container class="shell-omni" palette="light">
     <Omni.Heading>
@@ -25,37 +65,15 @@
         {/each}
     </Omni.Body>
 
-    <Omni.Body hidden="medium large">
-        <Popover.Container data-sizing="medium" id="landing-navigation-popover">
-            <Popover.Button palette="dark" variation="clear">LINKS</Popover.Button>
-
-            <Popover.Region>
-                <Menu.Container palette="light">
-                    {#each center as link (link.href)}
-                        <Menu.Anchor
-                            href={link.href}
-                            target={link.is_external ? "_blank" : undefined}
-                            rel={link.is_external ? "noopener noreferrer" : undefined}
-                            >{link.text}</Menu.Anchor
-                        >
-                    {/each}
-
-                    <Menu.Divider />
-
-                    {#each right as link (link.href)}
-                        <Menu.Anchor
-                            href={link.href}
-                            target={link.is_external ? "_blank" : undefined}
-                            rel={link.is_external ? "noopener noreferrer" : undefined}
-                            >{link.text}</Menu.Anchor
-                        >
-                    {/each}
-                </Menu.Container>
-            </Popover.Region>
-        </Popover.Container>
-    </Omni.Body>
-
     <Omni.Footer hidden="medium large" />
+
+    <Omni.Body hidden="medium large">
+        <Dialog.Button for="shell-omni-navigation" palette="dark" variation="clear">
+            <MenuIcon size="1.25em" />
+            <Spacer orientation="horizontal" spacing="tiny" />
+            MENU
+        </Dialog.Button>
+    </Omni.Body>
 
     <Omni.Footer hidden="tiny small">
         {#each right as link (link.href)}
@@ -75,5 +93,9 @@
 <style>
     :global(.shell-omni) {
         --omni-margin: 0;
+    }
+
+    :global(.shell-omni-body) {
+        --dialog-region-spacing: 0;
     }
 </style>

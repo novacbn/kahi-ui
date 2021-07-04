6fb67b26a67ded3aabcb81b8e5db97ee5796ee0e
novacbn <31122674+novacbn@users.noreply.github.com>
2021-07-04 17:39:21 -0400

    - Removed some old Storybook stuff

--- .storybook/main.cjs
+++ .storybook/main.cjs
@@ -4,10 +4,8 @@ module.exports = {
     core: {
         builder: "storybook-builder-vite",
     },
-    features: {
-        postcss: false,
-    },
+
     stories: ["../src/**/*.stories.mdx", "../src/**/*.stories.@(js|jsx|ts|tsx|svelte)"],
-    addons: ["@storybook/addon-links", "@storybook/addon-essentials"],
+    addons: ["@storybook/addon-essentials"],
     svelteOptions,
 };

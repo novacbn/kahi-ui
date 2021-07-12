b206f8bdbe28d0319cb84c78f585ca5e3fd49840
novacbn <31122674+novacbn@users.noreply.github.com>
2021-07-12 11:49:12 -0400

    - Updated typings on `Menu.Anchor` / `Menu.Button` / `Menu.Divider` / `Menu.Heading` / `Menu.Item` - Added property passthrough to parent `Menu.Item`

--- src/components/navigation/menu/MenuAnchor.svelte
+++ src/components/navigation/menu/MenuAnchor.svelte
@@ -1,12 +1,11 @@
 <script lang="ts">
     import type {DESIGN_HIDDEN_ARGUMENT} from "../../../lib/types/hidden";
-    import type {DESIGN_SPACING_ARGUMENT} from "../../../lib/types/spacings";
 
     import Anchor from "../anchor/Anchor.svelte";
 
     import MenuItem from "./MenuItem.svelte";
 
-    export let element: HTMLElement | null = null;
+    export let element: HTMLLIElement | null = null;
 
     let _class: string = "";
     export let id: string = "";
@@ -19,16 +18,6 @@
 
     export let hidden: DESIGN_HIDDEN_ARGUMENT = false;
 
-    export let margin: DESIGN_SPACING_ARGUMENT | undefined = undefined;
-
-    export let margin_x: DESIGN_SPACING_ARGUMENT | undefined = undefined;
-    export let margin_y: DESIGN_SPACING_ARGUMENT | undefined = undefined;
-
-    export let margin_top: DESIGN_SPACING_ARGUMENT | undefined = undefined;
-    export let margin_left: DESIGN_SPACING_ARGUMENT | undefined = undefined;
-    export let margin_bottom: DESIGN_SPACING_ARGUMENT | undefined = undefined;
-    export let margin_right: DESIGN_SPACING_ARGUMENT | undefined = undefined;
-
     export let active: boolean = false;
     export let disabled: boolean = false;
 
@@ -36,10 +25,9 @@
     export let href: string = "";
     export let rel: string = "";
     export let target: string = "";
-
 </script>
 
-<MenuItem bind:element>
+<MenuItem bind:element {...$$props}>
     <Anchor
         current={active ? "page" : undefined}
         {disabled}

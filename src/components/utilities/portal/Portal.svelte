852666d1a83587fa01cacb88e4264dc33ce02c7c
novacbn <31122674+novacbn@users.noreply.github.com>
2021-07-16 15:25:36 -0400

    - Fixed `Portal` mounting exception - Fixed `Portal` not removing children on deconstruction

--- src/components/utilities/portal/Portal.svelte
+++ src/components/utilities/portal/Portal.svelte
@@ -1,25 +1,38 @@
 <script lang="ts">
     // TODO: Stories
 
-    import {onMount} from "svelte";
+    import {onDestroy, onMount} from "svelte";
 
     export let element: HTMLElement | null = null;
 
     export let prepend = false;
     export let target: HTMLElement | string = typeof window !== "object" ? "" : document.body;
 
+    onDestroy(() => {
+        if (element) element.remove();
+    });
+
     onMount(() => {
-        const element = typeof target === "string" ? document.querySelector(target) : target;
         if (!element) {
+            // NOTE: Technically this should never go wrong, since `onMount` is called
+            // after the `HTMLElement` reference is assigned. However we should try to
+            // satisfy TypeScript
+
+            throw new TypeError(
+                "bad attribute 'element' on 'Portal' (failed to access container element)"
+            );
+        }
+
+        const queried_target = typeof target === "string" ? document.querySelector(target) : target;
+        if (!queried_target) {
             throw new ReferenceError(
                 `bad attribute 'target' on 'Portal' (selector '${target}' not found)`
             );
         }
 
-        if (prepend) element.prepend(element);
-        else element.append(element);
+        if (prepend) queried_target.prepend(element);
+        else queried_target.append(element);
     });
-
 </script>
 
 <div bind:this={element} class="portal">

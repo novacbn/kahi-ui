84f7873b8e63128b92e51ab1f54741152b1ca816
novacbn <31122674+novacbn@users.noreply.github.com>
2021-07-26 00:55:26 -0400

    - Migrated `Offscreen` / `Overlay` / `Popover` to `$$Events` / `$$Props` typings

--- src/components/overlays/popover/Popover.svelte
+++ src/components/overlays/popover/Popover.svelte
@@ -10,45 +10,61 @@
         DESIGN_ALIGNMENT_X_SINGULAR_ARGUMENT,
         DESIGN_ALIGNMENT_Y_SINGULAR_ARGUMENT,
     } from "../../../lib/types/alignments";
-    import {make_id_context} from "../../../lib/stores/id";
-    import {make_state_context} from "../../../lib/stores/state";
 
-    import type {DESIGN_HIDDEN_ARGUMENT} from "../../../lib/types/hidden";
+    import type {IGlobalProperties} from "../../../lib/types/global";
+    import type {IHTML5Properties} from "../../../lib/types/html5";
     import type {DESIGN_PLACEMENT_ARGUMENT} from "../../../lib/types/placements";
     import type {DESIGN_SPACING_SINGULAR_ARGUMENT} from "../../../lib/types/spacings";
 
     import {click_outside} from "../../../lib/actions/click_outside";
 
+    import {make_id_context} from "../../../lib/stores/id";
+    import {make_state_context} from "../../../lib/stores/state";
+
     import {map_data_attributes, map_global_attributes} from "../../../lib/util/attributes";
+
+    type $$Events = {
+        active: CustomEvent<void>;
+
+        dismiss: CustomEvent<void>;
+    };
+
+    type $$Props = {
+        element?: HTMLDivElement;
+
+        dismissible?: boolean;
+        logic_id?: string;
+        state?: boolean;
+
+        placement?: DESIGN_PLACEMENT_ARGUMENT;
+
+        alignment_x?: DESIGN_ALIGNMENT_X_SINGULAR_ARGUMENT;
+        alignment_y?: DESIGN_ALIGNMENT_Y_SINGULAR_ARGUMENT;
+
+        spacing?: DESIGN_SPACING_SINGULAR_ARGUMENT;
+    } & IHTML5Properties &
+        IGlobalProperties;
 
     const dispatch = createEventDispatcher();
 
-    export let element: HTMLElement | null = null;
+    export let element: $$Props["element"] = undefined;
 
-    let _class: string = "";
-    export let id: string = "";
-    export let name: string = "";
-    export let style: string = "";
-    export let tabindex: number | string = "";
-    export let title: string = "";
-
+    let _class: $$Props["class"] = "";
     export {_class as class};
 
-    export let hidden: DESIGN_HIDDEN_ARGUMENT = false;
+    export let dismissible: $$Props["dismissible"] = false;
+    export let logic_id: $$Props["logic_id"] = "";
+    export let state: $$Props["state"] = false;
 
-    export let dismissible: boolean = false;
-    export let logic_id: string = "";
-    export let state: boolean = false;
+    export let placement: $$Props["placement"] = undefined;
 
-    export let placement: DESIGN_PLACEMENT_ARGUMENT | undefined = undefined;
+    export let alignment_x: $$Props["alignment_x"] = undefined;
+    export let alignment_y: $$Props["alignment_y"] = undefined;
 
-    export let alignment_x: DESIGN_ALIGNMENT_X_SINGULAR_ARGUMENT | undefined = undefined;
-    export let alignment_y: DESIGN_ALIGNMENT_Y_SINGULAR_ARGUMENT | undefined = undefined;
+    export let spacing: $$Props["spacing"] = undefined;
 
-    export let spacing: DESIGN_SPACING_SINGULAR_ARGUMENT | undefined = undefined;
-
-    const _logic_id = make_id_context(logic_id);
-    const _state = make_state_context(state);
+    const _logic_id = make_id_context(logic_id as string);
+    const _state = make_state_context(state as boolean);
 
     let _previous_state = state;
 
@@ -60,7 +76,7 @@
         if (state && dismissible) state = false;
     }
 
-    $: $_state = state;
+    $: $_state = state as boolean;
 
     $: {
         if (_previous_state !== state) {
@@ -88,8 +104,8 @@
     {...map_data_attributes({
         "alignment-x": alignment_x,
         "alignment-y": alignment_y,
-        spacing,
         placement,
+        spacing,
     })}
     use:click_outside={{on_click_outside}}
 >

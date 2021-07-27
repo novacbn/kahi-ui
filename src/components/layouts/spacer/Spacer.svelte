5594a504fa417fc3e99029a6066660ed03cff555
novacbn <31122674+novacbn@users.noreply.github.com>
2021-07-27 00:10:33 -0400

    - Migrated `Group` / `Mosaic` / `Scrollable` / `Spacer` / `Stack` to `$$Props` typings

--- src/components/layouts/spacer/Spacer.svelte
+++ src/components/layouts/spacer/Spacer.svelte
@@ -1,27 +1,34 @@
 <script lang="ts">
+    import type {IGlobalProperties} from "../../../lib/types/global";
+    import type {IHTML5Properties} from "../../../lib/types/html5";
     import type {DESIGN_ORIENTATION_ARGUMENT} from "../../../lib/types/orientations";
     import type {DESIGN_SPACING_ARGUMENT} from "../../../lib/types/spacings";
 
     import {map_data_attributes, map_global_attributes} from "../../../lib/util/attributes";
 
-    export let element: HTMLElement | null = null;
+    type $$Props = {
+        element?: HTMLDivElement | HTMLSpanElement;
 
-    let _class: string = "";
-    export let id: string = "";
-    export let name: string = "";
-    export let style: string = "";
-    export let tabindex: number | string = "";
-    export let title: string = "";
+        orientation?: DESIGN_ORIENTATION_ARGUMENT;
+        variation?: "inline";
 
+        spacing?: DESIGN_SPACING_ARGUMENT;
+        spacing_x?: DESIGN_SPACING_ARGUMENT;
+        spacing_y?: DESIGN_SPACING_ARGUMENT;
+    } & IHTML5Properties &
+        IGlobalProperties;
+
+    export let element: $$Props["element"] = undefined;
+
+    let _class: $$Props["class"] = "";
     export {_class as class};
 
-    export let orientation: DESIGN_ORIENTATION_ARGUMENT | undefined = undefined;
-    export let variation: "inline" | undefined = undefined;
+    export let orientation: $$Props["orientation"] = undefined;
+    export let variation: $$Props["variation"] = undefined;
 
-    export let spacing: DESIGN_SPACING_ARGUMENT | undefined = undefined;
-    export let spacing_x: DESIGN_SPACING_ARGUMENT | undefined = undefined;
-    export let spacing_y: DESIGN_SPACING_ARGUMENT | undefined = undefined;
-
+    export let spacing: $$Props["spacing"] = undefined;
+    export let spacing_x: $$Props["spacing_x"] = undefined;
+    export let spacing_y: $$Props["spacing_y"] = undefined;
 </script>
 
 {#if variation === "inline"}
--- src/components/layouts/stack/Stack.svelte
+++ src/components/layouts/stack/Stack.svelte
@@ -4,65 +4,53 @@
         DESIGN_ALIGNMENT_X_ARGUMENT,
         DESIGN_ALIGNMENT_Y_ARGUMENT,
     } from "../../../lib/types/alignments";
-    import type {DESIGN_HIDDEN_ARGUMENT} from "../../../lib/types/hidden";
+    import type {IGlobalProperties} from "../../../lib/types/global";
+    import type {IHTML5Properties} from "../../../lib/types/html5";
     import type {DESIGN_ORIENTATION_VERTICAL_ARGUMENT} from "../../../lib/types/orientations";
-    import type {DESIGN_INTRINSIC_SIZING_ARGUMENT} from "../../../lib/types/sizings";
-    import type {DESIGN_SPACING_ARGUMENT} from "../../../lib/types/spacings";
+    import type {IIntrinsicProperties} from "../../../lib/types/sizings";
+    import type {
+        DESIGN_SPACING_ARGUMENT,
+        IMarginProperties,
+        IPaddingProperties,
+    } from "../../../lib/types/spacings";
     import type {DESIGN_FLEX_VARIATION_ARGUMENT} from "../../../lib/types/variations";
 
     import {map_data_attributes, map_global_attributes} from "../../../lib/util/attributes";
 
-    export let element: HTMLElement | null = null;
+    type $$Props = {
+        element?: HTMLDivElement;
 
-    let _class: string = "";
-    export let id: string = "";
-    export let name: string = "";
-    export let style: string = "";
-    export let tabindex: number | string = "";
-    export let title: string = "";
+        orientation?: DESIGN_ORIENTATION_VERTICAL_ARGUMENT;
+        variation?: DESIGN_FLEX_VARIATION_ARGUMENT;
 
+        alignment?: DESIGN_ALIGNMENT_ARGUMENT;
+        alignment_x?: DESIGN_ALIGNMENT_X_ARGUMENT;
+        alignment_y?: DESIGN_ALIGNMENT_Y_ARGUMENT;
+
+        spacing?: DESIGN_SPACING_ARGUMENT;
+        spacing_x?: DESIGN_SPACING_ARGUMENT;
+        spacing_y?: DESIGN_SPACING_ARGUMENT;
+    } & IHTML5Properties &
+        IGlobalProperties &
+        IIntrinsicProperties &
+        IMarginProperties &
+        IPaddingProperties;
+
+    export let element: $$Props["element"] = undefined;
+
+    let _class: $$Props["class"] = "";
     export {_class as class};
 
-    export let hidden: DESIGN_HIDDEN_ARGUMENT = false;
+    export let orientation: $$Props["orientation"] = undefined;
+    export let variation: $$Props["variation"] = undefined;
 
-    export let margin: DESIGN_SPACING_ARGUMENT | undefined = undefined;
+    export let alignment: $$Props["alignment"] = undefined;
+    export let alignment_x: $$Props["alignment_x"] = undefined;
+    export let alignment_y: $$Props["alignment_y"] = undefined;
 
-    export let margin_x: DESIGN_SPACING_ARGUMENT | undefined = undefined;
-    export let margin_y: DESIGN_SPACING_ARGUMENT | undefined = undefined;
-
-    export let margin_top: DESIGN_SPACING_ARGUMENT | undefined = undefined;
-    export let margin_left: DESIGN_SPACING_ARGUMENT | undefined = undefined;
-    export let margin_bottom: DESIGN_SPACING_ARGUMENT | undefined = undefined;
-    export let margin_right: DESIGN_SPACING_ARGUMENT | undefined = undefined;
-
-    export let padding: DESIGN_SPACING_ARGUMENT | undefined = undefined;
-
-    export let padding_x: DESIGN_SPACING_ARGUMENT | undefined = undefined;
-    export let padding_y: DESIGN_SPACING_ARGUMENT | undefined = undefined;
-
-    export let padding_top: DESIGN_SPACING_ARGUMENT | undefined = undefined;
-    export let padding_left: DESIGN_SPACING_ARGUMENT | undefined = undefined;
-    export let padding_bottom: DESIGN_SPACING_ARGUMENT | undefined = undefined;
-    export let padding_right: DESIGN_SPACING_ARGUMENT | undefined = undefined;
-
-    export let height: DESIGN_INTRINSIC_SIZING_ARGUMENT | undefined = undefined;
-    export let max_height: DESIGN_INTRINSIC_SIZING_ARGUMENT | undefined = undefined;
-    export let min_height: DESIGN_INTRINSIC_SIZING_ARGUMENT | undefined = undefined;
-
-    export let width: DESIGN_INTRINSIC_SIZING_ARGUMENT | undefined = undefined;
-    export let max_width: DESIGN_INTRINSIC_SIZING_ARGUMENT | undefined = undefined;
-    export let min_width: DESIGN_INTRINSIC_SIZING_ARGUMENT | undefined = undefined;
-
-    export let orientation: DESIGN_ORIENTATION_VERTICAL_ARGUMENT | undefined = undefined;
-    export let variation: DESIGN_FLEX_VARIATION_ARGUMENT | undefined = undefined;
-
-    export let alignment: DESIGN_ALIGNMENT_ARGUMENT | undefined = undefined;
-    export let alignment_x: DESIGN_ALIGNMENT_X_ARGUMENT | undefined = undefined;
-    export let alignment_y: DESIGN_ALIGNMENT_Y_ARGUMENT | undefined = undefined;
-
-    export let spacing: DESIGN_SPACING_ARGUMENT | undefined = undefined;
-    export let spacing_x: DESIGN_SPACING_ARGUMENT | undefined = undefined;
-    export let spacing_y: DESIGN_SPACING_ARGUMENT | undefined = undefined;
+    export let spacing: $$Props["spacing"] = undefined;
+    export let spacing_x: $$Props["spacing_x"] = undefined;
+    export let spacing_y: $$Props["spacing_y"] = undefined;
 </script>
 
 <div

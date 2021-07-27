6179e6809b4d2572a98dd2d55d16a5ca6ee75095
novacbn <31122674+novacbn@users.noreply.github.com>
2021-07-27 17:38:49 -0400

    - Added note about Deno detection

--- src/lib/util/environment.ts
+++ src/lib/util/environment.ts
@@ -1,1 +1,4 @@
+// TODO: Change to proper runtime detection. Some runtimes like Deno have
+// `window` polyfilled, so this would throw a false positive
+
 export const IS_BROWSER: boolean = typeof window !== "undefined";

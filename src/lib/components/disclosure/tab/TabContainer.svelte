9738c25e10a10814497c1b901411a7ae6cf49763
novacbn <31122674+novacbn@users.noreply.github.com>
2021-08-15 23:39:32 -0400

    - Initial `Tab` design

---
+++ src/lib/components/disclosure/tab/TabContainer.svelte
@@ -0,0 +1,27 @@
+<script lang="ts">
+    import type {IGlobalProperties} from "../../../types/global";
+    import type {IHTML5Properties} from "../../../types/html5";
+    import type {IMarginProperties} from "../../../types/spacings";
+
+    import {map_data_attributes, map_global_attributes} from "../../../util/attributes";
+
+    type $$Props = {
+        element?: HTMLSpanElement;
+    } & IHTML5Properties &
+        IGlobalProperties &
+        IMarginProperties;
+
+    export let element: $$Props["element"] = undefined;
+
+    let _class: $$Props["class"] = "";
+    export {_class as class};
+</script>
+
+<div
+    bind:this={element}
+    {...map_global_attributes($$props)}
+    class="tab {_class}"
+    {...map_data_attributes({})}
+>
+    <slot />
+</div>

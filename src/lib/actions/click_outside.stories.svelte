600a2f63108cc59aceeccb782362bb70bce6bdda
novacbn <31122674+novacbn@users.noreply.github.com>
2021-08-19 17:48:26 -0400

    - Added story for `click_outside` Svelte Action

---
+++ src/lib/actions/click_outside.stories.svelte
@@ -0,0 +1,33 @@
+<script>
+    import {Meta, Story, Template} from "@storybook/addon-svelte-csf";
+
+    import {click_outside} from "./click_outside";
+
+    function on_click(event) {
+        is_clicked = true;
+    }
+
+    function on_click_outside(event) {
+        is_clicked = false;
+    }
+
+    let is_clicked = false;
+</script>
+
+<Meta title="Actions/click_outside" />
+
+<Template>
+    <slot />
+</Template>
+
+<Story name="Default">
+    <div
+        class="box"
+        data-palette={is_clicked ? "affirmative" : "negative"}
+        data-padding="small"
+        on:click={on_click}
+        use:click_outside={{on_click_outside}}
+    >
+        {is_clicked ? "I was clicked inside!" : "I am currently not clicked..."}
+    </div>
+</Story>

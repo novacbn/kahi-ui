a7a05c0b2ec1b1f7cbc1fae046e7f1d8ca82c924
novacbn <31122674+novacbn@users.noreply.github.com>
2021-08-23 14:29:10 -0400

    - Fixed internal typings for a few Components

--- src/lib/components/feedback/wave/Wave.svelte
+++ src/lib/components/feedback/wave/Wave.svelte
@@ -2,8 +2,6 @@
     import type {IGlobalProperties} from "../../../types/global";
     import type {IHTML5Properties} from "../../../types/html5";
     import type {DESIGN_PALETTE_ARGUMENT} from "../../../types/palettes";
-    import type {DESIGN_POSITION_ARGUMENT} from "../../../types/positions";
-    import type {DESIGN_SIZE_ARGUMENT} from "../../../types/sizes";
     import type {IMarginProperties} from "../../../types/spacings";
 
     import {map_data_attributes, map_global_attributes} from "../../../util/attributes";
@@ -12,7 +10,6 @@
         element?: HTMLSpanElement;
 
         palette?: DESIGN_PALETTE_ARGUMENT;
-        position?: DESIGN_POSITION_ARGUMENT;
     } & IHTML5Properties &
         IGlobalProperties &
         IMarginProperties;

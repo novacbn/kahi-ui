a339c2baca29541b28334eb5a6e708f746de6cd2
novacbn <31122674+novacbn@users.noreply.github.com>
2021-08-31 20:25:09 -0400

    - Added `--font-content-size-local-*` / `--font-content-size-root-*` CSS variable variants

--- windi.config.cjs
+++ windi.config.cjs
@@ -163,6 +163,22 @@ export default defineConfig({
             },
         },
 
+        lfontSize: {
+            xs: ["0.75em", {lineHeight: "1em"}],
+            sm: ["0.875em", {lineHeight: "1.25em"}],
+            base: ["rem", {lineHeight: "1.5em"}],
+            lg: ["1.125em", {lineHeight: "1.75em"}],
+            xl: ["1.25em", {lineHeight: "1.75em"}],
+            "2xl": ["1.5em", {lineHeight: "2em"}],
+            "3xl": ["1.875em", {lineHeight: "2.25em"}],
+            "4xl": ["2.25em", {lineHeight: "2.5em"}],
+            "5xl": ["3em", {lineHeight: "1"}],
+            "6xl": ["3.75em", {lineHeight: "1"}],
+            "7xl": ["4.5em", {lineHeight: "1"}],
+            "8xl": ["6em", {lineHeight: "1"}],
+            "9xl": ["8em", {lineHeight: "1"}],
+        },
+
         lspacing: {
             // SOURCE: https://github.com/windicss/windicss/blob/0cac09dfbd630f391156e8426c5241fa5581a29b/src/config/base.ts#L51-L87
 

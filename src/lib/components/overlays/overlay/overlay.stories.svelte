69ac2072bdaf99f3bd1ff921b5f6c8fef0d35372
novacbn <31122674+novacbn@users.noreply.github.com>
2021-09-17 23:44:10 -0400

    - Migrated `Offscreen` / `Overlay` / `Popover` to CSF stories

---
+++ src/lib/components/overlays/overlay/overlay.stories.svelte
@@ -0,0 +1,39 @@
+<script>
+    import {Meta, Story, Template} from "@storybook/addon-svelte-csf";
+
+    import Button from "../../interactables/button/Button.svelte";
+    import * as Card from "../../surfaces/card";
+    import Code from "../../typography/code/Code.svelte";
+    import Text from "../../typography/text/Text.svelte";
+    import ContextButton from "../../utilities/contextbutton/ContextButton.svelte";
+
+    import Overlay from "./Overlay.svelte";
+</script>
+
+<Meta title="Overlays/Overlay" />
+
+<Template>
+    <slot />
+</Template>
+
+<Story name="Default">
+    <Button for="overlay-default-story" palette="accent">Open Modal</Button>
+
+    <Overlay logic_id="overlay-default-story" captive dismissible>
+        <Card.Container palette="auto" max_width="viewport-75">
+            <Card.Header>Are you sure?</Card.Header>
+
+            <Card.Section>
+                <Text>
+                    Are you sure you want to delete:
+                    <Code>important-file.docx</Code>?
+                </Text>
+            </Card.Section>
+
+            <Card.Footer>
+                <ContextButton palette="inverse" variation="clear">Cancel</ContextButton>
+                <Button palette="negative">Delete</Button>
+            </Card.Footer>
+        </Card.Container>
+    </Overlay>
+</Story>

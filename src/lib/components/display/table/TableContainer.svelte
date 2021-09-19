86e92f2c953dce531c719a4a088167e9f8fcc06c
novacbn <31122674+novacbn@users.noreply.github.com>
2021-09-19 00:05:42 -0400

    - Added `<Table.Container sizing="XXX">` support

--- src/lib/components/display/table/TableContainer.svelte
+++ src/lib/components/display/table/TableContainer.svelte
@@ -2,7 +2,7 @@
     import type {IGlobalProperties} from "../../../types/global";
     import type {IHTML5Properties} from "../../../types/html5";
     import type {DESIGN_PALETTE_ARGUMENT} from "../../../types/palettes";
-    import type {IIntrinsicProperties} from "../../../types/sizings";
+    import type {DESIGN_SIZING_ARGUMENT, IIntrinsicProperties} from "../../../types/sizings";
     import type {IMarginProperties, IPaddingProperties} from "../../../types/spacings";
     import type {DESIGN_TABLE_VARIATION_ARGUMENT} from "../../../types/variations";
 
@@ -12,6 +12,7 @@
         element?: HTMLTableElement;
 
         palette?: DESIGN_PALETTE_ARGUMENT;
+        sizing?: DESIGN_SIZING_ARGUMENT;
         variation?: DESIGN_TABLE_VARIATION_ARGUMENT;
     } & IHTML5Properties &
         IGlobalProperties &
@@ -26,13 +27,14 @@
     export let element: $$Props["element"] = undefined;
 
     export let palette: $$Props["palette"] = undefined;
+    export let sizing: $$Props["sizing"] = undefined;
     export let variation: $$Props["variation"] = undefined;
 </script>
 
 <table
     bind:this={element}
     {...map_global_attributes($$props)}
-    {...map_data_attributes({palette, variation})}
+    {...map_data_attributes({palette, sizing, variation})}
 >
     <slot />
 </table>

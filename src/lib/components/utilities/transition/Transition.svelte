4b37b32e4af30f0fa8fc20e5df499a71a9ac9110
novacbn <31122674+novacbn@users.noreply.github.com>
2021-09-23 22:30:38 -0400

    - Added initial `Transition` Utility Component implementation

---
+++ src/lib/components/utilities/transition/Transition.svelte
@@ -0,0 +1,40 @@
+<script lang="ts">
+    import {map_data_attributes} from "../../../util/attributes";
+
+    type $$Events = {
+        animationend: AnimationEvent;
+
+        animationstart: AnimationEvent;
+    };
+
+    type $$Props = {
+        element?: HTMLDivElement;
+
+        delay?: number | string;
+        direction?: "bottom" | "left" | "right" | "top";
+        state?: boolean;
+        transition?: "clip" | "scale" | "slide";
+    };
+
+    type $$Slots = {
+        default: {};
+    };
+
+    export let element: $$Props["element"] = undefined;
+
+    export let delay: $$Props["delay"] = undefined;
+    export let direction: $$Props["direction"] = undefined;
+    export let state: $$Props["state"] = undefined;
+    export let transition: $$Props["transition"] = undefined;
+</script>
+
+<div
+    bind:this={element}
+    class="transition"
+    {...map_data_attributes({direction, transition, state})}
+    on:animationend
+    on:animationstart
+    style={delay ? `--delay:${delay}` : ""}
+>
+    <slot />
+</div>
---
+++ src/lib/components/utilities/transition/transition.stories.svelte
@@ -0,0 +1,111 @@
+<script>
+    import {Meta, Story, Template} from "@storybook/addon-svelte-csf";
+
+    import Button from "../../interactables/button/Button.svelte";
+    import * as Grid from "../../layouts/grid";
+    import Box from "../../surfaces/box/Box.svelte";
+
+    import Transition from "./Transition.svelte";
+
+    const DIRECTIONS = [
+        ["bottom", true],
+        ["left", false],
+        ["right", false],
+        ["top", false],
+    ];
+
+    let state = false;
+
+    function on_state_click(event) {
+        state = !state;
+    }
+</script>
+
+<Meta title="Utilities/Transition" />
+
+<Template>
+    <slot />
+</Template>
+
+<Story name="Default">
+    <Button on:click={on_state_click}>Toggle State</Button>
+
+    <Transition transition="clip" {state}>
+        <Box class="box" padding="huge">hello world!</Box>
+    </Transition>
+</Story>
+
+<Story name="Clip">
+    <Button on:click={on_state_click}>Toggle State</Button>
+
+    <Grid.Container points={["4", "desktop:3", "tablet:2", "mobile:1"]} spacing="medium">
+        {#each DIRECTIONS as [direction, is_default] (direction)}
+            <Transition transition="clip" {direction} {state}>
+                <Box padding="huge">
+                    Clip {`${direction.toUpperCase()}${is_default ? " / DEFAULT" : ""}`}
+                </Box>
+            </Transition>
+        {/each}
+    </Grid.Container>
+</Story>
+
+<Story name="Clip Grid">
+    <Grid.Container points="4" spacing="medium">
+        {#each new Array(20) as _, index (index)}
+            <Transition
+                transition="clip"
+                direction="bottom"
+                delay={Math.floor(index / 4) * 0.5}
+                state={true}
+            >
+                <Box padding="large">hello world!</Box>
+            </Transition>
+        {/each}
+    </Grid.Container>
+</Story>
+
+<Story name="Scale">
+    <Button on:click={on_state_click}>Toggle State</Button>
+
+    <Transition transition="scale" {state}>
+        <Box padding="huge">hello world!</Box>
+    </Transition>
+</Story>
+
+<Story name="Scale Grid">
+    <Grid.Container points="4" spacing="medium">
+        {#each new Array(20) as _, index (index)}
+            <Transition transition="scale" delay={Math.floor(index / 4) * 0.5} state={true}>
+                <Box padding="large">hello world!</Box>
+            </Transition>
+        {/each}
+    </Grid.Container>
+</Story>
+
+<Story name="Slide">
+    <Button on:click={on_state_click} margin_bottom="huge">Toggle State</Button>
+
+    <Grid.Container
+        points={["4", "desktop:3", "tablet:2", "mobile:1"]}
+        spacing="medium"
+        margin_top="huge"
+    >
+        {#each DIRECTIONS as [direction, is_default] (direction)}
+            <Transition transition="slide" {direction} {state}>
+                <Box padding="huge">
+                    Slide {`${direction.toUpperCase()}${is_default ? " / DEFAULT" : ""}`}
+                </Box>
+            </Transition>
+        {/each}
+    </Grid.Container>
+</Story>
+
+<Story name="Slide Grid">
+    <Grid.Container points="4" spacing="medium">
+        {#each new Array(20) as _, index (index)}
+            <Transition transition="slide" delay={Math.floor(index / 4) * 0.5} state={true}>
+                <Box padding="large">hello world!</Box>
+            </Transition>
+        {/each}
+    </Grid.Container>
+</Story>

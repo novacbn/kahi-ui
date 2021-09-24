5343482dc03e307dccd586510697fa0cf12d7ca1
novacbn <31122674+novacbn@users.noreply.github.com>
2021-09-24 12:44:50 -0400

    - Added CSS Theming Variables for `Container` - Added stories for `Container`

---
+++ src/lib/components/layouts/container/container.stories.svelte
@@ -0,0 +1,53 @@
+<script>
+    import {Meta, Story, Template} from "@storybook/addon-svelte-csf";
+
+    import Text from "../../typography/text/Text.svelte";
+
+    import Stack from "../stack/Stack.svelte";
+
+    import Container from "./Container.svelte";
+
+    const VIEWPORTS = [
+        ["prose", true],
+        ["mobile", false],
+        ["tablet", false],
+        ["desktop", false],
+        ["widescreen", false],
+    ];
+</script>
+
+<Meta title="Layouts/Container" />
+
+<Template>
+    <slot />
+</Template>
+
+<Story name="Default">
+    <Container>
+        <Text>
+            Lorem ipsum dolor sit amet, consectetur adipiscing elit. Proin et consectetur orci.
+            Curabitur a egestas turpis, vitae convallis sapien. Sed pellentesque rutrum tellus, in
+            iaculis dolor tincidunt non. Orci varius natoque penatibus et magnis dis parturient
+            montes, nascetur ridiculus mus.
+        </Text>
+    </Container>
+</Story>
+
+<Story name="Viewport">
+    <Stack spacing="medium">
+        {#each VIEWPORTS as [viewport, is_default] (viewport)}
+            <Container {viewport}>
+                <Text is="strong">
+                    {`${viewport.toUpperCase()}${is_default ? " / DEFAULT" : ""}`}
+                </Text>
+
+                <Text>
+                    Lorem ipsum dolor sit amet, consectetur adipiscing elit. Proin et consectetur
+                    orci. Curabitur a egestas turpis, vitae convallis sapien. Sed pellentesque
+                    rutrum tellus, in iaculis dolor tincidunt non. Orci varius natoque penatibus et
+                    magnis dis parturient montes, nascetur ridiculus mus.
+                </Text>
+            </Container>
+        {/each}
+    </Stack>
+</Story>

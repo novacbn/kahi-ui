dfeb2ce6d02fd3aeae4b48e804a506be7dcefb9c
novacbn <31122674+novacbn@users.noreply.github.com>
2021-09-24 00:06:22 -0400

    - Cleaned up `Transition` implementation

--- src/lib/components/utilities/transition/Transition.svelte
+++ src/lib/components/utilities/transition/Transition.svelte
@@ -1,5 +1,8 @@
 <script lang="ts">
-    import {map_data_attributes} from "../../../util/attributes";
+    import type {IGlobalProperties} from "../../../types/global";
+    import type {IHTML5Properties} from "../../../types/html5";
+
+    import {map_data_attributes, map_global_attributes} from "../../../util/attributes";
 
     type $$Events = {
         animationend: AnimationEvent;
@@ -14,13 +17,19 @@
         direction?: "bottom" | "left" | "right" | "top";
         state?: boolean;
         transition?: "clip" | "scale" | "slide";
-    };
+    } & IHTML5Properties &
+        IGlobalProperties;
 
     type $$Slots = {
         default: {};
     };
 
     export let element: $$Props["element"] = undefined;
+
+    let _class: $$Props["class"] = "";
+    export let style: $$Props["style"] = "";
+
+    export {_class as class};
 
     export let delay: $$Props["delay"] = undefined;
     export let direction: $$Props["direction"] = undefined;
@@ -30,11 +39,12 @@
 
 <div
     bind:this={element}
-    class="transition"
+    {...map_global_attributes($$props)}
+    class="transition {_class}"
     {...map_data_attributes({direction, transition, state})}
     on:animationend
     on:animationstart
-    style={delay ? `--delay:${delay}` : ""}
+    style={delay ? `${style}--delay:${delay};` : style}
 >
     <slot />
 </div>

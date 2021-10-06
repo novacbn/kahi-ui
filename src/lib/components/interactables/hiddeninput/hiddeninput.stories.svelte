33a3c9203be485a3e62aec680001756ceb9b71ac
novacbn <31122674+novacbn@users.noreply.github.com>
2021-10-06 14:16:52 -0400

    - Added `HiddenInput` story

---
+++ src/lib/components/interactables/hiddeninput/hiddeninput.stories.svelte
@@ -0,0 +1,19 @@
+<script>
+    import {Meta, Story, Template} from "@storybook/addon-svelte-csf";
+
+    import Box from "../../surfaces/box/Box.svelte";
+
+    import HiddenInput from "./HiddenInput.svelte";
+</script>
+
+<Meta title="Interactables/HiddenInput" />
+
+<Template>
+    <slot />
+</Template>
+
+<Story name="Default">
+    <Box palette="negative">View the sibling element below this one in devtools.</Box>
+
+    <HiddenInput value="This is a HiddenInput" />
+</Story>

0ac85124bbfc10a59c3ca4decf3a52d8a7c8c96c
novacbn <31122674+novacbn@users.noreply.github.com>
2021-10-06 13:08:22 -0400

    - Added `Center` Component for quick flexbox centering - Added `Position` Component for quick transform related positioning

---
+++ src/lib/components/layouts/center/center.stories.svelte
@@ -0,0 +1,27 @@
+<script>
+    import {Meta, Story, Template} from "@storybook/addon-svelte-csf";
+
+    import Box from "../../surfaces/box/Box.svelte";
+    import Text from "../../typography/text/Text.svelte";
+
+    import Center from "./Center.svelte";
+</script>
+
+<Meta title="Layouts/Center" />
+
+<Template>
+    <slot />
+</Template>
+
+<Story name="Default">
+    <Box palette="inverse">
+        <Center>
+            <Text style="max-width:25ch;">
+                Lorem ipsum dolor sit amet, consectetur adipiscing elit. Proin et consectetur orci.
+                Curabitur a egestas turpis, vitae convallis sapien. Sed pellentesque rutrum tellus,
+                in iaculis dolor tincidunt non. Orci varius natoque penatibus et magnis dis
+                parturient montes, nascetur ridiculus mus.
+            </Text>
+        </Center>
+    </Box>
+</Story>

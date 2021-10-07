b84c5ee1b19f5526dbda6b96d6dc317507ec6943
novacbn <31122674+novacbn@users.noreply.github.com>
2021-10-06 23:21:55 -0400

    - Migrated `Divider`, `Grid`, `Group`, `Mosaic`, `Spacer`, `Stack` stories - Added `Scrollable` story

---
+++ src/lib/components/layouts/mosaic/mosaic.stories.svelte
@@ -0,0 +1,106 @@
+<script>
+    import {Meta, Story, Template} from "@storybook/addon-svelte-csf";
+
+    import Box from "../../surfaces/box/Box.svelte";
+    import Text from "../../typography/text/Text.svelte";
+
+    import Stack from "../stack/Stack.svelte";
+
+    import Mosaic from "./Mosaic.svelte";
+
+    const SIZINGS = [
+        ["none", true],
+        ["tiny", false],
+        ["small", false],
+        ["medium", false],
+        ["large", false],
+        ["huge", false],
+    ];
+
+    const SPACINGS = [
+        ["none", true],
+        ["tiny", false],
+        ["small", false],
+        ["medium", false],
+        ["large", false],
+        ["huge", false],
+    ];
+</script>
+
+<Meta title="Layouts/Mosaic" />
+
+<Template>
+    <slot />
+</Template>
+
+<Story name="Default">
+    <Mosaic sizing="huge" spacing="medium">
+        <Box palette="alert" style="height:3rem;" />
+        <Box palette="affirmative" style="height:3rem;" />
+        <Box palette="negative" style="height:3rem;" />
+
+        <Box palette="affirmative" style="height:3rem;" />
+        <Box palette="negative" style="height:3rem;" />
+        <Box palette="alert" style="height:3rem;" />
+
+        <Box palette="alert" style="height:3rem;" />
+        <Box palette="affirmative" style="height:3rem;" />
+        <Box palette="negative" style="height:3rem;" />
+
+        <Box palette="negative" style="height:3rem;" />
+        <Box palette="alert" style="height:3rem;" />
+        <Box palette="affirmative" style="height:3rem;" />
+
+        <Box palette="affirmative" style="height:3rem;" />
+        <Box palette="negative" style="height:3rem;" />
+        <Box palette="alert" style="height:3rem;" />
+
+        <Box palette="negative" style="height:3rem;" />
+        <Box palette="alert" style="height:3rem;" />
+        <Box palette="affirmative" style="height:3rem;" />
+
+        <Box palette="alert" style="height:3rem;" />
+        <Box palette="affirmative" style="height:3rem;" />
+        <Box palette="negative" style="height:3rem;" />
+    </Mosaic>
+</Story>
+
+<Story name="Sizing">
+    <Stack orientation="horizontal" alignment_y="top" spacing="medium" variation="wrap">
+        {#each SIZINGS as [sizing, is_default]}
+            <div>
+                <Text is="strong">
+                    {`${sizing.toUpperCase()}${is_default ? " / DEFAULT" : ""}`}
+                </Text>
+
+                <Box palette="inverse" padding="small">
+                    <Mosaic {sizing}>
+                        <Box palette="alert" style="height:3rem;" />
+                        <Box palette="affirmative" style="height:3rem;" />
+                        <Box palette="negative" style="height:3rem;" />
+                    </Mosaic>
+                </Box>
+            </div>
+        {/each}
+    </Stack>
+</Story>
+
+<Story name="Spacing">
+    <Stack orientation="horizontal" alignment_y="top" spacing="medium" variation="wrap">
+        {#each SPACINGS as [spacing, is_default]}
+            <div>
+                <Text is="strong">
+                    {`${spacing.toUpperCase()}${is_default ? " / DEFAULT" : ""}`}
+                </Text>
+
+                <Box palette="inverse" padding="small">
+                    <Mosaic {spacing}>
+                        <Box palette="alert" style="height:3rem;" />
+                        <Box palette="affirmative" style="height:3rem;" />
+                        <Box palette="negative" style="height:3rem;" />
+                    </Mosaic>
+                </Box>
+            </div>
+        {/each}
+    </Stack>
+</Story>
---
+++ src/lib/components/layouts/spacer/spacer.stories.svelte
@@ -0,0 +1,77 @@
+<script>
+    import {Meta, Story, Template} from "@storybook/addon-svelte-csf";
+
+    import Box from "../../surfaces/box/Box.svelte";
+    import Text from "../../typography/text/Text.svelte";
+
+    import Stack from "../stack/Stack.svelte";
+
+    import Spacer from "./Spacer.svelte";
+
+    const SPACINGS = [
+        ["none", true],
+        ["tiny", false],
+        ["small", false],
+        ["medium", false],
+        ["large", false],
+        ["huge", false],
+    ];
+</script>
+
+<Meta title="Layouts/Spacer" />
+
+<Template>
+    <slot />
+</Template>
+
+<Story name="Default">
+    <Box palette="inverse" padding="small">
+        <Stack orientation="horizontal">
+            LEFT
+            <Spacer />
+            RIGHT
+        </Stack>
+    </Box>
+</Story>
+
+<Story name="Variation">
+    <Stack orientation="horizontal" alignment_y="top" spacing="medium" variation="wrap">
+        <div>
+            <Text is="strong">BLOCK / DEFAULT</Text>
+
+            <Box palette="dark" margin_top="small" padding="small">
+                TOP
+                <Spacer spacing="huge" />
+                BOTTOM
+            </Box>
+        </div>
+
+        <div>
+            <Text is="strong">INLINE</Text>
+
+            <Box palette="dark" margin_top="small" padding="small">
+                LEFT
+                <Spacer variation="inline" orientation="horizontal" spacing="huge" />
+                RIGHT
+            </Box>
+        </div>
+    </Stack>
+</Story>
+
+<Story name="Spacing">
+    <Stack orientation="horizontal" spacing="medium" variation="wrap">
+        {#each SPACINGS as [spacing, is_default]}
+            <div>
+                <Text is="strong">
+                    {`${spacing.toUpperCase()}${is_default ? " / DEFAULT" : ""}`}
+                </Text>
+
+                <Box palette="inverse" padding="small">
+                    TOP
+                    <Spacer {spacing} />
+                    BOTTOM
+                </Box>
+            </div>
+        {/each}
+    </Stack>
+</Story>

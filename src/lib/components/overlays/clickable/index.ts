bf4568d2bd61d5d9ee955efc1ef1513ba825aedb
novacbn <31122674+novacbn@users.noreply.github.com>
2021-10-10 00:18:13 -0400

    - Updated exports

--- src/lib/components/overlays/clickable/index.ts
+++ src/lib/components/overlays/clickable/index.ts
@@ -1,3 +1,4 @@
 export {default as Container} from "./ClickableContainer.svelte";
 
-export {default as Item} from "./ClickableItem.svelte";
+export {default as Anchor} from "./ClickableAnchor.svelte";
+export {default as Label} from "./ClickableLabel.svelte";

e87e15545a33aa871dced8711817b999f163a59f
novacbn <31122674+novacbn@users.noreply.github.com>
2021-10-10 00:17:36 -0400

    - Migrated `Clickable.Item` -> `Clickable.Anchor` / `Clickable.Label` - Migrated `Clickable.Label` to use new context handling

--- src/lib/components/overlays/clickable/ClickableAnchor.svelte
+++ src/lib/components/overlays/clickable/ClickableAnchor.svelte
@@ -14,13 +14,11 @@
     };
 
     type $$Props = {
-        element?: HTMLAnchorElement | HTMLLabelElement;
+        element?: HTMLAnchorElement;
 
         active?: boolean;
         current?: ARIA_CURRENT_ARGUMENT;
         disabled?: boolean;
-
-        for?: string;
 
         download?: string;
         href?: string;
@@ -42,35 +40,19 @@
     export let current: $$Props["current"] = undefined;
     export let disabled: $$Props["disabled"] = undefined;
 
-    let _for: $$Props["for"] = undefined;
-    export {_for as for};
-
     export let download: $$Props["download"] = undefined;
     export let href: $$Props["href"] = undefined;
     export let rel: $$Props["rel"] = undefined;
     export let target: $$Props["target"] = undefined;
 </script>
 
-{#if href}
-    <a
-        bind:this={element}
-        {...map_global_attributes($$props)}
-        class="clickable-item {_class}"
-        {...map_aria_attributes({pressed: active, current, disabled})}
-        {...map_attributes({download, href, rel, target})}
-        on:click
-    >
-        <slot />
-    </a>
-{:else}
-    <label
-        bind:this={element}
-        {...map_global_attributes($$props)}
-        class="clickable-item {_class}"
-        {...map_attributes({for: _for})}
-        {...map_aria_attributes({disabled, pressed: active})}
-        on:click
-    >
-        <slot />
-    </label>
-{/if}
+<a
+    bind:this={element}
+    {...map_global_attributes($$props)}
+    class="clickable-item {_class}"
+    {...map_aria_attributes({pressed: active, current, disabled})}
+    {...map_attributes({download, href, rel, target})}
+    on:click
+>
+    <slot />
+</a>

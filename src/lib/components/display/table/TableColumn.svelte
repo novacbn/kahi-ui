18a1bb9fd78660811a2fcfbc31ed4b5fa861bfff
novacbn <31122674+novacbn@users.noreply.github.com>
2021-10-17 17:46:42 -0400

    - Added bindings to `colspan` / `rowspan` - Changed from deprecated `HTMLTableDataCellElement` / `HTMLTableHeaderCellElement` -> `HTMLTableCellElement`

--- src/lib/components/display/table/TableColumn.svelte
+++ src/lib/components/display/table/TableColumn.svelte
@@ -4,10 +4,13 @@
     import type {IIntrinsicProperties} from "../../../types/sizings";
     import type {IMarginProperties, IPaddingProperties} from "../../../types/spacings";
 
-    import {map_global_attributes} from "../../../util/attributes";
+    import {map_attributes, map_global_attributes} from "../../../util/attributes";
 
     type $$Props = {
-        element?: HTMLTableDataCellElement;
+        element?: HTMLTableCellElement;
+
+        colspan?: number | string;
+        rowspan?: number | string;
     } & IHTML5Properties &
         IGlobalProperties &
         IIntrinsicProperties &
@@ -19,8 +22,11 @@
     };
 
     export let element: $$Props["element"] = undefined;
+
+    export let colspan: $$Props["colspan"] = undefined;
+    export let rowspan: $$Props["rowspan"] = undefined;
 </script>
 
-<td bind:this={element} {...map_global_attributes($$props)}>
+<td bind:this={element} {...map_global_attributes($$props)} {...map_attributes({colspan, rowspan})}>
     <slot />
 </td>

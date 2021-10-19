8e71c331e0d44c72af91a463936d5512b8b06883
novacbn <31122674+novacbn@users.noreply.github.com>
2021-10-19 12:30:34 -0400

    - Updated `Accordion` to use `display: none` on non `Transition` content - Updated `Accordion` to integrate with `Transition` internal CSS properties

--- src/lib/components/disclosure/accordion/Accordion.stories.svelte
+++ src/lib/components/disclosure/accordion/Accordion.stories.svelte
@@ -5,6 +5,8 @@
     import Transition from "../../utilities/transition/Transition.svelte";
 
     import * as Accordion from "./index";
+
+    const ACCORDIONS = ["One", "Two", "Three"];
 </script>
 
 <Meta title="Disclosure/Accordion" />
@@ -15,98 +17,106 @@
 
 <Story name="Default">
     <Accordion.Container logic_name="accordion-default">
-        <Accordion.Group logic_id="accordion-1">
-            <Accordion.Label>Section 1</Accordion.Label>
+        <Accordion.Group logic_id="accordion-default-1">
+            <Accordion.Label>Section One</Accordion.Label>
 
             <Accordion.Section>
-                <Transition animation="slide" direction="top" variation="enter">
-                    <Text>
-                        Lorem ipsum dolor sit amet, consectetur adipiscing elit. Proin et
-                        consectetur orci. Curabitur a egestas turpis, vitae convallis sapien. Sed
-                        pellentesque rutrum tellus, in iaculis dolor tincidunt non. Orci varius
-                        natoque penatibus et magnis dis parturient montes, nascetur ridiculus mus.
-                    </Text>
-                </Transition>
+                <Text>
+                    Lorem ipsum dolor sit amet, consectetur adipiscing elit. Proin et consectetur
+                    orci. Curabitur a egestas turpis, vitae convallis sapien. Sed pellentesque
+                    rutrum tellus, in iaculis dolor tincidunt non. Orci varius natoque penatibus et
+                    magnis dis parturient montes, nascetur ridiculus mus.
+                </Text>
             </Accordion.Section>
         </Accordion.Group>
 
-        <Accordion.Group logic_id="accordion-2">
-            <Accordion.Label>Section 2</Accordion.Label>
+        <Accordion.Group logic_id="accordion-default-2">
+            <Accordion.Label>Section Two</Accordion.Label>
 
             <Accordion.Section>
-                <Transition animation="slide" direction="top" variation="enter">
-                    <Text>
-                        Lorem ipsum dolor sit amet, consectetur adipiscing elit. Proin et
-                        consectetur orci. Curabitur a egestas turpis, vitae convallis sapien. Sed
-                        pellentesque rutrum tellus, in iaculis dolor tincidunt non. Orci varius
-                        natoque penatibus et magnis dis parturient montes, nascetur ridiculus mus.
-                    </Text>
-                </Transition>
+                <Text>
+                    Lorem ipsum dolor sit amet, consectetur adipiscing elit. Proin et consectetur
+                    orci. Curabitur a egestas turpis, vitae convallis sapien. Sed pellentesque
+                    rutrum tellus, in iaculis dolor tincidunt non. Orci varius natoque penatibus et
+                    magnis dis parturient montes, nascetur ridiculus mus.
+                </Text>
             </Accordion.Section>
         </Accordion.Group>
 
-        <Accordion.Group logic_id="accordion-3">
-            <Accordion.Label>Section 3</Accordion.Label>
+        <Accordion.Group logic_id="accordion-default-3">
+            <Accordion.Label>Section Three</Accordion.Label>
 
             <Accordion.Section>
-                <Transition animation="slide" direction="top" variation="enter">
-                    <Text>
-                        Lorem ipsum dolor sit amet, consectetur adipiscing elit. Proin et
-                        consectetur orci. Curabitur a egestas turpis, vitae convallis sapien. Sed
-                        pellentesque rutrum tellus, in iaculis dolor tincidunt non. Orci varius
-                        natoque penatibus et magnis dis parturient montes, nascetur ridiculus mus.
-                    </Text>
-                </Transition>
+                <Text>
+                    Lorem ipsum dolor sit amet, consectetur adipiscing elit. Proin et consectetur
+                    orci. Curabitur a egestas turpis, vitae convallis sapien. Sed pellentesque
+                    rutrum tellus, in iaculis dolor tincidunt non. Orci varius natoque penatibus et
+                    magnis dis parturient montes, nascetur ridiculus mus.
+                </Text>
             </Accordion.Section>
         </Accordion.Group>
     </Accordion.Container>
 </Story>
 
 <Story name="Inclusive">
-    <Accordion.Container behavior="inclusive" logic_name="accordion-default">
-        <Accordion.Group logic_id="accordion-1">
-            <Accordion.Label>Section 1</Accordion.Label>
+    <Accordion.Container behavior="inclusive" logic_name="accordion-inclusive">
+        {#each ACCORDIONS as name, index}
+            <Accordion.Group logic_id="accordion-inclusive-{index + 1}">
+                <Accordion.Label>Section {name}</Accordion.Label>
 
-            <Accordion.Section>
-                <Transition animation="slide" direction="top" variation="enter">
+                <Accordion.Section>
                     <Text>
                         Lorem ipsum dolor sit amet, consectetur adipiscing elit. Proin et
                         consectetur orci. Curabitur a egestas turpis, vitae convallis sapien. Sed
                         pellentesque rutrum tellus, in iaculis dolor tincidunt non. Orci varius
                         natoque penatibus et magnis dis parturient montes, nascetur ridiculus mus.
                     </Text>
-                </Transition>
-            </Accordion.Section>
-        </Accordion.Group>
+                </Accordion.Section>
+            </Accordion.Group>
+        {/each}
+    </Accordion.Container>
+</Story>
 
-        <Accordion.Group logic_id="accordion-2">
-            <Accordion.Label>Section 2</Accordion.Label>
+<Story name="Transition + Exclusive">
+    <Accordion.Container logic_name="accordion-transition-exclusive">
+        {#each ACCORDIONS as name, index}
+            <Accordion.Group logic_id="accordion-transition-exclusive-{index + 1}">
+                <Accordion.Label>Section {name}</Accordion.Label>
 
-            <Accordion.Section>
-                <Transition animation="slide" direction="top" variation="enter">
-                    <Text>
-                        Lorem ipsum dolor sit amet, consectetur adipiscing elit. Proin et
-                        consectetur orci. Curabitur a egestas turpis, vitae convallis sapien. Sed
-                        pellentesque rutrum tellus, in iaculis dolor tincidunt non. Orci varius
-                        natoque penatibus et magnis dis parturient montes, nascetur ridiculus mus.
-                    </Text>
-                </Transition>
-            </Accordion.Section>
-        </Accordion.Group>
+                <Accordion.Section>
+                    <Transition animation="slide" direction="top">
+                        <Text>
+                            Lorem ipsum dolor sit amet, consectetur adipiscing elit. Proin et
+                            consectetur orci. Curabitur a egestas turpis, vitae convallis sapien.
+                            Sed pellentesque rutrum tellus, in iaculis dolor tincidunt non. Orci
+                            varius natoque penatibus et magnis dis parturient montes, nascetur
+                            ridiculus mus.
+                        </Text>
+                    </Transition>
+                </Accordion.Section>
+            </Accordion.Group>
+        {/each}
+    </Accordion.Container>
+</Story>
 
-        <Accordion.Group logic_id="accordion-3">
-            <Accordion.Label>Section 3</Accordion.Label>
+<Story name="Transition + Inclusive">
+    <Accordion.Container behavior="inclusive" logic_name="accordion-transition-inclusive">
+        {#each ACCORDIONS as name, index}
+            <Accordion.Group logic_id="accordion-transition-inclusive-{index + 1}">
+                <Accordion.Label>Section {name}</Accordion.Label>
 
-            <Accordion.Section>
-                <Transition animation="slide" direction="top" variation="enter">
-                    <Text>
-                        Lorem ipsum dolor sit amet, consectetur adipiscing elit. Proin et
-                        consectetur orci. Curabitur a egestas turpis, vitae convallis sapien. Sed
-                        pellentesque rutrum tellus, in iaculis dolor tincidunt non. Orci varius
-                        natoque penatibus et magnis dis parturient montes, nascetur ridiculus mus.
-                    </Text>
-                </Transition>
-            </Accordion.Section>
-        </Accordion.Group>
+                <Accordion.Section>
+                    <Transition animation="slide" direction="top">
+                        <Text>
+                            Lorem ipsum dolor sit amet, consectetur adipiscing elit. Proin et
+                            consectetur orci. Curabitur a egestas turpis, vitae convallis sapien.
+                            Sed pellentesque rutrum tellus, in iaculis dolor tincidunt non. Orci
+                            varius natoque penatibus et magnis dis parturient montes, nascetur
+                            ridiculus mus.
+                        </Text>
+                    </Transition>
+                </Accordion.Section>
+            </Accordion.Group>
+        {/each}
     </Accordion.Container>
 </Story>

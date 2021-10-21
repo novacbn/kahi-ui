7789735e4ff1ef7e5ac71eab391ec177507edb44
novacbn <31122674+novacbn@users.noreply.github.com>
2021-10-21 16:00:20 -0400

    - Add initial `Carousel` Component implementation

---
+++ src/lib/components/disclosure/carousel/Carousel.stories.svelte
@@ -0,0 +1,49 @@
+<script>
+    import {Meta, Story, Template} from "@storybook/addon-svelte-csf";
+    import Center from "../../layouts/center/Center.svelte";
+    import Box from "../../surfaces/box/Box.svelte";
+
+    import * as Carousel from "./index";
+
+    const SECTIONS = ["negative", "affirmative", "alert"];
+</script>
+
+<Meta title="Disclosure/Carousel" />
+
+<Template>
+    <slot />
+</Template>
+
+<Story name="Default">
+    <Carousel.Container style="width:300px;height:200px;">
+        <Carousel.Section>
+            <Box palette="negative">
+                <Center height="100">300 x 200</Center>
+            </Box>
+        </Carousel.Section>
+
+        <Carousel.Section>
+            <Box palette="affirmative">
+                <Center height="100">300 x 200</Center>
+            </Box>
+        </Carousel.Section>
+
+        <Carousel.Section>
+            <Box palette="alert">
+                <Center height="100">300 x 200</Center>
+            </Box>
+        </Carousel.Section>
+    </Carousel.Container>
+</Story>
+
+<Story name="Vertical">
+    <Carousel.Container orientation="vertical" style="width:300px;height:200px;">
+        {#each SECTIONS as palette}
+            <Carousel.Section>
+                <Box {palette}>
+                    <Center height="100">300 x 200</Center>
+                </Box>
+            </Carousel.Section>
+        {/each}
+    </Carousel.Container>
+</Story>
---
+++ src/lib/components/disclosure/carousel/index.ts
@@ -0,0 +1,3 @@
+export {default as Container} from "./CarouselContainer.svelte";
+
+export {default as Section} from "./CarouselSection.svelte";

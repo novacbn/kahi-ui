7f87281e0093aa0212a6ebeefdd0118704f220a2
novacbn <31122674+novacbn@users.noreply.github.com>
2021-10-25 14:27:58 -0400

    - Migrated `Aside` / `Omni` to CSF format

---
+++ src/lib/components/navigation/aside/Aside.stories.svelte
@@ -0,0 +1,131 @@
+<script>
+    import {Meta, Story, Template} from "@storybook/addon-svelte-csf";
+
+    import Divider from "../../layouts/divider/Divider.svelte";
+    import Mosaic from "../../layouts/mosaic/Mosaic.svelte";
+    import Spacer from "../../layouts/spacer/Spacer.svelte";
+    import Text from "../../typography/text/Text.svelte";
+    import ContextButton from "../../utilities/contextbutton/ContextButton.svelte";
+
+    import Anchor from "../anchor/Anchor.svelte";
+    import * as Menu from "../menu";
+
+    import * as Aside from "./";
+
+    const PALETTES = [
+        ["default", true],
+        ["accent", false],
+        ["auto", false],
+        ["inverse", false],
+        ["dark", false],
+        ["light", false],
+        ["alert", false],
+        ["affirmative", false],
+        ["negative", false],
+    ];
+</script>
+
+<Meta title="Navigation/Aside" />
+
+<Template>
+    <slot />
+</Template>
+
+<Story name="Default">
+    <Aside.Container
+        logic_id="aside-default"
+        palette="dark"
+        max_width="content-max"
+        height="viewport-100"
+        captive
+        dismissible
+    >
+        <Aside.Header>
+            <Anchor href="#">Kahi UI</Anchor>
+            <Divider />
+        </Aside.Header>
+
+        <Aside.Section>
+            <Menu.Container>
+                <Menu.Heading>DISPLAY</Menu.Heading>
+
+                <Menu.Button>
+                    Badge
+                    <Spacer />
+                    <span>ICON</span>
+                </Menu.Button>
+
+                <Menu.Heading>FEEDBACK</Menu.Heading>
+
+                <Menu.Button>
+                    Dot
+                    <Spacer />
+                    <span>ICON</span>
+                </Menu.Button>
+
+                <Menu.Button active>
+                    Spinner
+                    <Spacer />
+                    <span>ICON</span>
+                </Menu.Button>
+            </Menu.Container>
+        </Aside.Section>
+
+        <Aside.Footer>
+            <Anchor href="#">
+                <Text is="small">v0.2.0</Text>
+            </Anchor>
+        </Aside.Footer>
+
+        <svelte:fragment slot="open">
+            <ContextButton variation="clear">+</ContextButton>
+        </svelte:fragment>
+
+        <svelte:fragment slot="close">
+            <ContextButton palette="dark" variation="clear">x</ContextButton>
+        </svelte:fragment>
+    </Aside.Container>
+</Story>
+
+<Story name="Palette">
+    <Mosaic sizing="medium" spacing="medium">
+        {#each PALETTES as [palette, is_default]}
+            <Aside.Container palette={palette === "default" ? undefined : palette}>
+                <Aside.Header>
+                    Kahi UI
+                    <Divider />
+                </Aside.Header>
+
+                <Aside.Section>
+                    <Menu.Container>
+                        <Menu.Heading>DISPLAY</Menu.Heading>
+
+                        <Menu.Button>
+                            Badge
+                            <Spacer />
+                            <span>ICON</span>
+                        </Menu.Button>
+
+                        <Menu.Heading>FEEDBACK</Menu.Heading>
+
+                        <Menu.Button>
+                            Dot
+                            <Spacer />
+                            <span>ICON</span>
+                        </Menu.Button>
+
+                        <Menu.Button>
+                            Spinner
+                            <Spacer />
+                            <span>ICON</span>
+                        </Menu.Button>
+                    </Menu.Container>
+                </Aside.Section>
+
+                <Aside.Footer>
+                    {`${palette.toUpperCase()}${is_default ? " / DEFAULT" : ""}`}
+                </Aside.Footer>
+            </Aside.Container>
+        {/each}
+    </Mosaic>
+</Story>
---
+++ src/lib/components/navigation/omni/Omni.stories.svelte
@@ -0,0 +1,92 @@
+<script>
+    import {Meta, Story, Template} from "@storybook/addon-svelte-csf";
+
+    import Button from "../../interactables/button/Button.svelte";
+    import Divider from "../../layouts/divider/Divider.svelte";
+    import Spacer from "../../layouts/spacer/Spacer.svelte";
+    import Stack from "../../layouts/stack/Stack.svelte";
+    import Text from "../../typography/text/Text.svelte";
+    import ContextButton from "../../utilities/contextbutton/ContextButton.svelte";
+
+    import Anchor from "../anchor/Anchor.svelte";
+    import * as Menu from "../menu";
+
+    import * as Omni from "./";
+
+    const PALETTES = [
+        ["default", true],
+        ["accent", false],
+        ["auto", false],
+        ["inverse", false],
+        ["dark", false],
+        ["light", false],
+        ["alert", false],
+        ["affirmative", false],
+        ["negative", false],
+    ];
+</script>
+
+<Meta title="Navigation/Omni" />
+
+<Template>
+    <slot />
+</Template>
+
+<Story name="Default">
+    <Omni.Container logic_id="omni-default" palette="dark" width="viewport-100" captive dismissible>
+        <Omni.Header>
+            <Anchor href="#">Kahi UI</Anchor>
+            <Divider orientation="vertical" />
+            <Anchor href="#">
+                <Text is="small">v0.2.0</Text>
+            </Anchor>
+
+            <Spacer />
+            <ContextButton palette="light" variation="clear">+</ContextButton>
+            <ContextButton palette="light" variation="clear">x</ContextButton>
+        </Omni.Header>
+
+        <Omni.Footer>
+            <Omni.Section>
+                <Menu.Container orientation={["desktop:horizontal", "widescreen:horizontal"]}>
+                    <Menu.Button palette="light" variation="clear" active>Docs</Menu.Button>
+                    <Menu.Button palette="light" variation="clear">Playground</Menu.Button>
+                    <Menu.Button palette="light" variation="clear">Storybook</Menu.Button>
+                </Menu.Container>
+            </Omni.Section>
+
+            <Omni.Section>
+                <Menu.Container orientation={["desktop:horizontal", "widescreen:horizontal"]}>
+                    <Menu.Button palette="light" variation="clear">GitHub</Menu.Button>
+                </Menu.Container>
+            </Omni.Section>
+        </Omni.Footer>
+    </Omni.Container>
+</Story>
+
+<Story name="Palette">
+    <Stack orientation="horizontal" spacing="medium" variation="wrap">
+        {#each PALETTES as [palette, is_default]}
+            <Omni.Container
+                max_width="content-max"
+                palette={palette === "default" ? undefined : palette}
+            >
+                <Omni.Header>
+                    Kahi UI
+                    <Divider orientation="vertical" />
+                    <Text is="small">
+                        {`${palette.toUpperCase()}${is_default ? " / DEFAULT" : ""}`}
+                    </Text>
+                </Omni.Header>
+
+                <Omni.Footer>
+                    <Omni.Section>
+                        <Menu.Container>
+                            <Menu.Button variation="clear">GitHub</Menu.Button>
+                        </Menu.Container>
+                    </Omni.Section>
+                </Omni.Footer>
+            </Omni.Container>
+        {/each}
+    </Stack>
+</Story>

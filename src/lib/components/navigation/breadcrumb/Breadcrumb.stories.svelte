7b8de64fc9a6ba71be495ea87d71b52d7a83b526
novacbn <31122674+novacbn@users.noreply.github.com>
2021-10-25 14:30:38 -0400

    - Migrated `Breadcrumb` to CSF format

---
+++ src/lib/components/navigation/breadcrumb/Breadcrumb.stories.svelte
@@ -0,0 +1,27 @@
+<script>
+    import {Meta, Story, Template} from "@storybook/addon-svelte-csf";
+
+    import * as Breadcrumb from "./";
+</script>
+
+<Meta title="Navigation/Breadcrumb" />
+
+<Template>
+    <slot />
+</Template>
+
+<Story name="Default">
+    <Breadcrumb.Container>
+        <Breadcrumb.Anchor href="#">Home</Breadcrumb.Anchor>
+        <Breadcrumb.Anchor href="#">Contacts</Breadcrumb.Anchor>
+        <Breadcrumb.Anchor href="#" active>John Doe</Breadcrumb.Anchor>
+    </Breadcrumb.Container>
+</Story>
+
+<Story name="Separator">
+    <Breadcrumb.Container separator=">">
+        <Breadcrumb.Anchor href="#">Home</Breadcrumb.Anchor>
+        <Breadcrumb.Anchor href="#">Contacts</Breadcrumb.Anchor>
+        <Breadcrumb.Anchor href="#" active>John Doe</Breadcrumb.Anchor>
+    </Breadcrumb.Container>
+</Story>

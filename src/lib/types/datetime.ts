52f41c1a3abde5a9662f952d08af7135722acd10
novacbn <31122674+novacbn@users.noreply.github.com>
2021-11-04 13:00:14 -0400

    - Fixed date range calculation for `get_calendar_dates` - Updated `get_calendar_dates` to split into weeks - Renamed `get_calendar_day` -> `get_calendar_dates` to reflect Temporal API

---
+++ src/lib/types/datetime.ts
@@ -0,0 +1,4 @@
+/**
+ * Represents a property that is either a UNIX Epoch timestamp in milliseconds or a ISO 8601 timestamp
+ */
+export type PROPERTY_TIMESTAMP = number | string;
--- src/lib/util/datetime.ts
+++ src/lib/util/datetime.ts
@@ -1,5 +1,11 @@
 import {Temporal} from "@js-temporal/polyfill";
 
+import {wrap} from "./math";
+
+function get_calendar_day(date: Temporal.PlainDate): number {
+    return wrap(date.dayOfWeek + 1, 1, date.daysInWeek);
+}
+
 function get_timestamp_date(timestamp: number | string): Temporal.PlainDate {
     if (typeof timestamp === "number") {
         const instant = Temporal.Instant.fromEpochMilliseconds(timestamp);
@@ -10,24 +16,39 @@ function get_timestamp_date(timestamp: number | string): Temporal.PlainDate {
     return Temporal.PlainDate.from(timestamp);
 }
 
-export function get_calendar_days(timestamp: number | string): Temporal.PlainDate[] {
+export function get_calendar_dates(timestamp: number | string): Temporal.PlainDate[][] {
     const date = get_timestamp_date(timestamp);
 
-    let starting_date = Temporal.PlainDate.from({year: date.year, month: date.month, day: 1});
-    starting_date = starting_date.subtract({days: starting_date.dayOfWeek});
+    let starting_date = Temporal.PlainDate.from({
+        year: date.year,
+        month: date.month,
+        day: 1,
+    });
+    starting_date = starting_date.subtract({
+        days: get_calendar_day(starting_date) - 1,
+    });
 
     let ending_date = Temporal.PlainDate.from({
         year: date.year,
         month: date.month,
         day: date.daysInMonth,
     });
-    ending_date = ending_date.add({days: ending_date.daysInWeek - (date.dayOfWeek - 1)});
+    ending_date = ending_date.add({
+        days: ending_date.daysInWeek - get_calendar_day(ending_date) + 1,
+    });
 
     const duration = starting_date.until(ending_date);
 
     return new Array(duration.total({unit: "day"}))
         .fill(null)
-        .map((_, index) => starting_date.add({days: index}));
+        .map((_, index) => starting_date.add({days: index}))
+        .reduce<Temporal.PlainDate[][]>((accum, date, index) => {
+            const week_index = Math.floor(index / date.daysInWeek);
+            if (!accum[week_index]) accum[week_index] = [];
+
+            accum[week_index].push(date);
+            return accum;
+        }, []);
 }
 
 export function get_calendar_months(timestamp: number | string): Temporal.PlainYearMonth[] {

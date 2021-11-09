8d4ef87c5b89b69133d288e1391a7d94c864392d
novacbn <31122674+novacbn@users.noreply.github.com>
2021-11-09 13:36:45 -0500

    - Simplified `Spacer` CSS' styling - Migrated `Spacer` from Windi CSS - Removed `<Spacer orientation>`, and added deprecation notice     - `<Spacer spacing>` is now always applied for both axis     - Use `<Spacer spacing_x/spacing_y>` instead for per-axis spacing

--- src/lib/components/layouts/spacer/Spacer.svelte
+++ src/lib/components/layouts/spacer/Spacer.svelte
@@ -9,6 +9,8 @@
     type $$Props = {
         element?: HTMLDivElement | HTMLSpanElement;
 
+        is?: "div" | "span";
+
         orientation?: PROPERTY_ORIENTATION_BREAKPOINT;
         variation?: "inline";
 
@@ -24,10 +26,18 @@
 
     export let element: $$Props["element"] = undefined;
 
+    export let is: $$Props["is"] = "div";
+
     let _class: $$Props["class"] = "";
     export {_class as class};
 
+    /**
+     * @deprecated Use `<Spacer spacing_x="...">` / `<Spacer spacing_y="...">` instead.
+     */
     export let orientation: $$Props["orientation"] = undefined;
+    /**
+     * @deprecated Use `<Spacer is="span">` instead.
+     */
     export let variation: $$Props["variation"] = undefined;
 
     export let spacing: $$Props["spacing"] = undefined;
@@ -35,13 +45,12 @@
     export let spacing_y: $$Props["spacing_y"] = undefined;
 </script>
 
-{#if variation === "inline"}
+{#if is === "span" || variation === "inline"}
     <span
         bind:this={element}
         {...map_global_attributes($$props)}
         class="spacer {_class}"
         {...map_data_attributes({
-            orientation,
             spacing,
             "spacing-x": spacing_x,
             "spacing-y": spacing_y,
@@ -55,7 +64,6 @@
         {...map_global_attributes($$props)}
         class="spacer {_class}"
         {...map_data_attributes({
-            orientation,
             spacing,
             "spacing-x": spacing_x,
             "spacing-y": spacing_y,

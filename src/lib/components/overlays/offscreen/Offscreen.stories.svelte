2fd4f330bcc67d91e7bcb866dacd107ed0f586f6
NovacBN <novacbn@gmail.com>
2021-12-08 22:30:50 -0500

    [0.4.13] Add `<Offscreen/Overlay loading="lazy">` Support (#91)
    * - Added `<Offscreen/Overlay loading="lazy">` support
    
    * - Fixed using wrong enum

--- src/lib/components/overlays/offscreen/Offscreen.stories.svelte
+++ src/lib/components/overlays/offscreen/Offscreen.stories.svelte
@@ -1,10 +1,11 @@
 <script>
     import {Meta, Story, Template} from "@storybook/addon-svelte-csf";
 
-    import Box from "../../surfaces/box/Box.svelte";
     import Button from "../../interactables/button/Button.svelte";
-    import ContextButton from "../../utilities/contextbutton/ContextButton.svelte";
     import Stack from "../../layouts/stack/Stack.svelte";
+    import Box from "../../surfaces/box/Box.svelte";
+    import Code from "../../typography/code/Code.svelte";
+    import ContextButton from "../../utilities/contextbutton/ContextButton.svelte";
 
     import Offscreen from "./Offscreen.svelte";
 
@@ -35,6 +36,23 @@
     </Offscreen>
 </Story>
 
+<Story name="Lazy">
+    <Box palette="negative" margin_bottom="medium">
+        To view this property in action, open devtools and watch the <Code>div</Code> elements' contents.
+    </Box>
+
+    <Button for="offscreen-once" palette="accent">Open LAZY Offscreen</Button>
+
+    <Offscreen logic_id="offscreen-once" placement="top" loading="lazy" hidden>
+        <Box palette="accent" padding="medium">
+            <Stack orientation="horizontal" alignment_y="center" spacing="medium">
+                <ContextButton palette="light" variation="clear">X</ContextButton>
+                Lazy Offscreen
+            </Stack>
+        </Box>
+    </Offscreen>
+</Story>
+
 <Story name="Once">
     <Button for="offscreen-once" palette="accent">Open ONCE Offscreen</Button>
 
@@ -42,7 +60,7 @@
         <Box palette="accent" padding="medium">
             <Stack orientation="horizontal" alignment_y="center" spacing="medium">
                 <ContextButton palette="light" variation="clear">X</ContextButton>
-                I am hidden on all Viewports!
+                Once Offscreen
             </Stack>
         </Box>
     </Offscreen>

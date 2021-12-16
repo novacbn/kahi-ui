83495a5ddb74594c4545ada782ab02c7e1e50ec2
novacbn <31122674+novacbn@users.noreply.github.com>
2021-12-15 22:16:05 -0500

    - Moved layout data attributes handling to `<Overlay.Section>` - Integrated `.transition` class and attribute handling into `<Overlay.Section>` for better flexbox handling

--- src/lib/components/overlays/overlay/OverlayContainer.svelte
+++ src/lib/components/overlays/overlay/OverlayContainer.svelte
@@ -1,24 +1,16 @@
 <script lang="ts">
     import {createEventDispatcher} from "svelte";
 
-    import type {
-        PROPERTY_ALIGNMENT_BREAKPOINT,
-        PROPERTY_ALIGNMENT_X_BREAKPOINT,
-        PROPERTY_ALIGNMENT_Y_BREAKPOINT,
-    } from "../../../types/alignments";
     import type {PROPERTY_BEHAVIOR_LOADING_LAZY} from "../../../types/behaviors";
     import type {IGlobalProperties} from "../../../types/global";
     import type {IHTML5Events, IHTML5Properties} from "../../../types/html5";
-    import type {PROPERTY_ORIENTATION_Y_BREAKPOINT} from "../../../types/orientations";
     import type {PROPERTY_REFERENCE_TARGET} from "../../../types/references";
-    import type {IPaddingProperties, PROPERTY_SPACING_BREAKPOINT} from "../../../types/spacings";
+    import type {IPaddingProperties} from "../../../types/spacings";
 
-    import {auto_focus} from "../../../actions/auto_focus";
     import type {IForwardedActions} from "../../../actions/forward_actions";
     import {forward_actions} from "../../../actions/forward_actions";
-    import {trap_focus} from "../../../actions/trap_focus";
 
-    import {map_data_attributes, map_global_attributes} from "../../../util/attributes";
+    import {map_global_attributes} from "../../../util/attributes";
     import {action_exit} from "../../../util/keybind";
 
     import Backdrop from "../backdrop/Backdrop.svelte";
@@ -46,16 +38,6 @@
         focus_first?: PROPERTY_REFERENCE_TARGET;
         focus_last?: PROPERTY_REFERENCE_TARGET;
         focus_target?: PROPERTY_REFERENCE_TARGET;
-
-        orientation?: PROPERTY_ORIENTATION_Y_BREAKPOINT;
-
-        alignment?: PROPERTY_ALIGNMENT_BREAKPOINT;
-        alignment_x?: PROPERTY_ALIGNMENT_X_BREAKPOINT;
-        alignment_y?: PROPERTY_ALIGNMENT_Y_BREAKPOINT;
-
-        spacing?: PROPERTY_SPACING_BREAKPOINT;
-        spacing_x?: PROPERTY_SPACING_BREAKPOINT;
-        spacing_y?: PROPERTY_SPACING_BREAKPOINT;
     } & IHTML5Properties &
         IGlobalProperties &
         IPaddingProperties;
@@ -83,16 +65,6 @@
     export let dismissible: $$Props["dismissible"] = undefined;
     export let loading: $$Props["loading"] = undefined;
     export let once: $$Props["once"] = undefined;
-
-    export let orientation: $$Props["orientation"] = undefined;
-
-    export let alignment: $$Props["alignment"] = undefined;
-    export let alignment_x: $$Props["alignment_x"] = undefined;
-    export let alignment_y: $$Props["alignment_y"] = undefined;
-
-    export let spacing: $$Props["spacing"] = undefined;
-    export let spacing_x: $$Props["spacing_x"] = undefined;
-    export let spacing_y: $$Props["spacing_y"] = undefined;
 
     function on_group_change(event: CustomEvent<void>): void {
         dispatch(logic_state ? "active" : "dismiss");
@@ -123,15 +95,6 @@
     bind:this={element}
     {...map_global_attributes($$props)}
     class="overlay {_class}"
-    {...map_data_attributes({
-        alignment,
-        "alignment-x": alignment_x,
-        "alignment-y": alignment_y,
-        orientation,
-        spacing,
-        "spacing-x": spacing_x,
-        "spacing-y": spacing_y,
-    })}
     use:forward_actions={{actions}}
     on:click
     on:contextmenu
--- src/lib/components/overlays/overlay/OverlaySection.svelte
+++ src/lib/components/overlays/overlay/OverlaySection.svelte
@@ -1,10 +1,20 @@
 <script lang="ts">
+    import type {
+        PROPERTY_ALIGNMENT_BREAKPOINT,
+        PROPERTY_ALIGNMENT_X_BREAKPOINT,
+        PROPERTY_ALIGNMENT_Y_BREAKPOINT,
+    } from "../../../types/alignments";
     import {TOKENS_BEHAVIOR_LOADING} from "../../../types/behaviors";
     import type {PROPERTY_DIRECTIONS} from "../../../types/directions";
     import type {IGlobalProperties} from "../../../types/global";
     import type {IHTML5Events, IHTML5Properties} from "../../../types/html5";
+    import type {PROPERTY_ORIENTATION_Y_BREAKPOINT} from "../../../types/orientations";
     import type {ISizeProperties} from "../../../types/sizes";
-    import type {IMarginProperties, IPaddingProperties} from "../../../types/spacings";
+    import type {
+        IMarginProperties,
+        IPaddingProperties,
+        PROPERTY_SPACING_BREAKPOINT,
+    } from "../../../types/spacings";
     import type {PROPERTY_TRANSITION_NAMES} from "../../../types/transitions";
     import {TOKENS_TRANSITION_NAMES} from "../../../types/transitions";
 
@@ -14,9 +24,7 @@
     import {forward_actions} from "../../../actions/forward_actions";
     import {trap_focus} from "../../../actions/trap_focus";
 
-    import {map_global_attributes} from "../../../util/attributes";
-
-    import Transition from "../../utilities/transition/Transition.svelte";
+    import {map_data_attributes, map_global_attributes} from "../../../util/attributes";
 
     import {
         CONTEXT_OVERLAY_FOCUS_FIRST,
@@ -40,6 +48,16 @@
 
         animation?: PROPERTY_TRANSITION_NAMES;
         direction?: PROPERTY_DIRECTIONS;
+
+        orientation?: PROPERTY_ORIENTATION_Y_BREAKPOINT;
+
+        alignment?: PROPERTY_ALIGNMENT_BREAKPOINT;
+        alignment_x?: PROPERTY_ALIGNMENT_X_BREAKPOINT;
+        alignment_y?: PROPERTY_ALIGNMENT_Y_BREAKPOINT;
+
+        spacing?: PROPERTY_SPACING_BREAKPOINT;
+        spacing_x?: PROPERTY_SPACING_BREAKPOINT;
+        spacing_y?: PROPERTY_SPACING_BREAKPOINT;
     } & IHTML5Properties &
         IGlobalProperties &
         IMarginProperties &
@@ -53,8 +71,21 @@
     export let actions: $$Props["actions"] = undefined;
     export let element: $$Props["element"] = undefined;
 
+    let _class: $$Props["class"] = "";
+    export {_class as class};
+
     export let animation: $$Props["animation"] = undefined;
     export let direction: $$Props["direction"] = undefined;
+
+    export let orientation: $$Props["orientation"] = undefined;
+
+    export let alignment: $$Props["alignment"] = undefined;
+    export let alignment_x: $$Props["alignment_x"] = undefined;
+    export let alignment_y: $$Props["alignment_y"] = undefined;
+
+    export let spacing: $$Props["spacing"] = undefined;
+    export let spacing_x: $$Props["spacing_x"] = undefined;
+    export let spacing_y: $$Props["spacing_y"] = undefined;
 
     const _overlay_id = CONTEXT_OVERLAY_ID.get();
     const _overlay_state = CONTEXT_OVERLAY_STATE.get();
@@ -76,6 +107,18 @@
 <section
     bind:this={element}
     {...map_global_attributes($$props)}
+    class="{_overlay_id ? 'transition' : ''} {_class}"
+    {...map_data_attributes({
+        alignment,
+        animation: _overlay_id ? animation ?? TOKENS_TRANSITION_NAMES.scale : undefined,
+        "alignment-x": alignment_x,
+        "alignment-y": alignment_y,
+        direction: _overlay_id ? direction : undefined,
+        orientation,
+        spacing,
+        "spacing-x": spacing_x,
+        "spacing-y": spacing_y,
+    })}
     use:click_inside={{
         ignore: _overlay_id ? `label[for="${$_overlay_id}"]` : undefined,
         on_click_inside: on_once,
@@ -104,19 +147,11 @@
     on:pointermove
     on:pointerout
     on:pointerup
+    on:animationend
+    on:animationstart
 >
-    {#if _overlay_id}
-        <Transition
-            animation={animation ?? TOKENS_TRANSITION_NAMES.scale}
-            {direction}
-            on:animationend
-            on:animationstart
-        >
-            {#if (_overlay_state && $_overlay_state) || !_overlay_loading || $_overlay_loading !== TOKENS_BEHAVIOR_LOADING.lazy}
-                <slot />
-            {/if}
-        </Transition>
-    {:else}
+    <!-- NOTE: Bit confusing, but basically checking if the `Overlay` is active / state unavailable or loading mode isn't lazy -->
+    {#if !_overlay_id || (_overlay_state && $_overlay_state) || !_overlay_loading || $_overlay_loading !== TOKENS_BEHAVIOR_LOADING.lazy}
         <slot />
     {/if}
 </section>

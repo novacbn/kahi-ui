1275ccb68e44f275b5c213795d3669ecaa7f0394
novacbn <31122674+novacbn@users.noreply.github.com>
2021-12-20 15:11:45 -0500

    - Converted `Omni` to using theming variables - Revamped layout management for `Omni` - Removed built-in viewport-based content collapsing from `Omni` in favor of composibility

--- src/lib/components/navigation/omni/Omni.stories.svelte
+++ src/lib/components/navigation/omni/Omni.stories.svelte
@@ -31,34 +31,28 @@
     <slot />
 </Template>
 
-<Story name="Default">
-    <Omni.Container logic_id="omni-default" palette="dark" width="viewport-100" captive dismissible>
+<Story name="Preview">
+    <Omni.Container palette="dark" width="viewport-100">
         <Omni.Header>
             <Anchor href="#">Kahi UI</Anchor>
             <Divider orientation="vertical" />
             <Anchor href="#">
-                <Text is="small">v0.2.0</Text>
+                <Text is="small">v0.5.0</Text>
             </Anchor>
-
-            <Spacer />
-            <ContextButton palette="light" variation="clear">+</ContextButton>
-            <ContextButton palette="light" variation="clear">x</ContextButton>
         </Omni.Header>
 
-        <Omni.Footer>
-            <Omni.Section>
-                <Menu.Container orientation={["desktop:horizontal", "widescreen:horizontal"]}>
-                    <Menu.Button palette="light" variation="clear" active>Docs</Menu.Button>
-                    <Menu.Button palette="light" variation="clear">Playground</Menu.Button>
-                    <Menu.Button palette="light" variation="clear">Storybook</Menu.Button>
-                </Menu.Container>
-            </Omni.Section>
+        <Omni.Section>
+            <Menu.Container orientation={["desktop:horizontal", "widescreen:horizontal"]}>
+                <Menu.Button palette="light" variation="clear" active>Docs</Menu.Button>
+                <Menu.Button palette="light" variation="clear">Playground</Menu.Button>
+                <Menu.Button palette="light" variation="clear">Storybook</Menu.Button>
+            </Menu.Container>
+        </Omni.Section>
 
-            <Omni.Section>
-                <Menu.Container orientation={["desktop:horizontal", "widescreen:horizontal"]}>
-                    <Menu.Button palette="light" variation="clear">GitHub</Menu.Button>
-                </Menu.Container>
-            </Omni.Section>
+        <Omni.Footer>
+            <Menu.Container orientation={["desktop:horizontal", "widescreen:horizontal"]}>
+                <Menu.Button palette="light" variation="clear">GitHub</Menu.Button>
+            </Menu.Container>
         </Omni.Footer>
     </Omni.Container>
 </Story>
@@ -79,11 +73,9 @@
                 </Omni.Header>
 
                 <Omni.Footer>
-                    <Omni.Section>
-                        <Menu.Container>
-                            <Menu.Button variation="clear">GitHub</Menu.Button>
-                        </Menu.Container>
-                    </Omni.Section>
+                    <Menu.Container>
+                        <Menu.Button variation="clear">GitHub</Menu.Button>
+                    </Menu.Container>
                 </Omni.Footer>
             </Omni.Container>
         {/each}
--- src/lib/components/navigation/omni/OmniContainer.svelte
+++ src/lib/components/navigation/omni/OmniContainer.svelte
@@ -1,42 +1,16 @@
 <script lang="ts">
-    /**
-     * TODO: Now that `Popover` exists, need to redo the collapse functionality of
-     * `Omni.Container` to something specialized. Thinking a full-screen blinder that
-     * uses `clip-path` as the effect.
-     *
-     * Ideally it's something that should be convertable into a standalone Overlays Component
-     * so it's reusable elsewhere as a wrapper transition. However with `Omni`, the wrapper would
-     * have to be nested in the `Omni.Container` layout. Rather than used as a wrapper like how
-     * `Aside.Container` is set up...
-     */
-
     import type {IGlobalProperties} from "../../../types/global";
-    import type {IHTML5Properties} from "../../../types/html5";
+    import type {IHTML5Events, IHTML5Properties} from "../../../types/html5";
     import type {PROPERTY_PALETTE} from "../../../types/palettes";
     import type {ISizeProperties} from "../../../types/sizes";
     import type {IMarginProperties, IPaddingProperties} from "../../../types/spacings";
 
-    import {make_id_context} from "../../../stores/id";
-    import {make_state_context} from "../../../stores/state";
-    import {viewports} from "../../../stores/viewport";
-
     import {map_data_attributes, map_global_attributes} from "../../../util/attributes";
 
-    import ContextBackdrop from "../../utilities/contextbackdrop/ContextBackdrop.svelte";
-
-    type $$Events = {
-        active: CustomEvent<void>;
-
-        dismiss: CustomEvent<void>;
-    };
+    type $$Events = IHTML5Events;
 
     type $$Props = {
         element?: HTMLElement;
-
-        captive?: boolean;
-        dismissible?: boolean;
-        logic_id?: string;
-        state?: boolean;
 
         palette?: PROPERTY_PALETTE;
         variation?: "sticky";
@@ -55,39 +29,9 @@
     let _class: $$Props["class"] = "";
     export {_class as class};
 
-    export let captive: $$Props["captive"] = undefined;
-    export let dismissible: $$Props["dismissible"] = undefined;
-    export let logic_id: $$Props["logic_id"] = "";
-    export let state: $$Props["state"] = undefined;
-
     export let palette: $$Props["palette"] = undefined;
     export let variation: $$Props["variation"] = undefined;
-
-    const _logic_id = make_id_context(logic_id as string);
-    const _state = make_state_context(state as boolean);
-    const _viewports = viewports({mobile: true, tablet: true});
-
-    function on_input(event: Event) {
-        state = (event.target as HTMLInputElement).checked;
-    }
-
-    $: if (!$_viewports) state = false;
-    $: $_state = state as boolean;
 </script>
-
-{#if $_logic_id}
-    <input
-        role="presentation"
-        id={$_logic_id}
-        type="checkbox"
-        bind:checked={$_state}
-        on:input={on_input}
-    />
-
-    {#if captive}
-        <ContextBackdrop {dismissible} />
-    {/if}
-{/if}
 
 <nav
     bind:this={element}

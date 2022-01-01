40509d8622cfa390df3f3b57c575f1196f5b3e1a
novacbn <31122674+novacbn@users.noreply.github.com>
2022-01-01 01:03:45 -0500

    - Modernized `Backdrop` to follow newer Component conventions

--- src/lib/components/overlays/backdrop/Backdrop.svelte
+++ src/lib/components/overlays/backdrop/Backdrop.svelte
@@ -1,24 +1,24 @@
 <script lang="ts">
     import type {IGlobalProperties} from "../../../types/global";
-    import type {IHTML5Properties} from "../../../types/html5";
-    import type {ISizeProperties} from "../../../types/sizes";
+    import type {IHTML5Events, IHTML5Properties} from "../../../types/html5";
 
     import {behavior_button} from "../../../actions/behavior_button";
+    import type {IForwardedActions} from "../../../actions/forward_actions";
+    import {forward_actions} from "../../../actions/forward_actions";
 
     import {map_global_attributes} from "../../../util/attributes";
 
-    type $$Events = {
-        click: MouseEvent;
-    };
+    type $$Events = IHTML5Events;
 
     type $$Props = {
+        actions?: IForwardedActions;
         element?: HTMLDivElement | HTMLLabelElement;
 
         for?: string;
     } & IHTML5Properties &
-        IGlobalProperties &
-        ISizeProperties;
+        IGlobalProperties;
 
+    export let actions: $$Props["actions"] = undefined;
     export let element: $$Props["element"] = undefined;
 
     let _class: $$Props["class"] = "";
@@ -36,13 +36,41 @@
         class="backdrop {_class}"
         for={_for}
         use:behavior_button={{enabled: true}}
+        use:forward_actions={{actions}}
         on:click
+        on:contextmenu
+        on:dblclick
+        on:focusin
+        on:focusout
+        on:keydown
+        on:keyup
+        on:pointercancel
+        on:pointerdown
+        on:pointerenter
+        on:pointerleave
+        on:pointermove
+        on:pointerout
+        on:pointerup
     />
 {:else}
     <div
         bind:this={element}
         {...map_global_attributes($$props)}
         class="backdrop {_class}"
+        use:forward_actions={{actions}}
         on:click
+        on:contextmenu
+        on:dblclick
+        on:focusin
+        on:focusout
+        on:keydown
+        on:keyup
+        on:pointercancel
+        on:pointerdown
+        on:pointerenter
+        on:pointerleave
+        on:pointermove
+        on:pointerout
+        on:pointerup
     />
 {/if}

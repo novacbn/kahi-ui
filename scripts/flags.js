9635b8707eb769de6479eef1f0600c7816a29e79
novacbn <31122674+novacbn@users.noreply.github.com>
2022-01-20 05:37:23 -0500

    - Added `Group` / `List` build flags

--- scripts/flags.js
+++ scripts/flags.js
@@ -14,9 +14,11 @@ function make_flag_map(argv, ...flags) {
 
 export const BUILD_FLAGS = make_flag_map(
     argv,
+    "--disable-components-display-list",
     "--disable-components-embedded-figure",
     "--disable-components-interactables-button",
     "--disable-components-layouts-center",
+    "--disable-components-layouts-group",
     "--disable-components-layouts-stack",
     "--disable-components-surfaces-box",
     "--disable-components-surfaces-card",

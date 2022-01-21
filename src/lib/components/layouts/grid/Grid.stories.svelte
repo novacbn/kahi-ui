076088a5f7679c279bd1e8ab297a5502c3b0085f
novacbn <31122674+novacbn@users.noreply.github.com>
2022-01-21 10:43:03 -0500

    - Fixed up `Grid` stories to use new icon sizes as height

--- src/lib/components/layouts/grid/Grid.stories.svelte
+++ src/lib/components/layouts/grid/Grid.stories.svelte
@@ -27,33 +27,33 @@
 
 <Story name="Preview">
     <Grid.Container points={["6", "mobile:3", "tablet:4", "desktop:5"]} spacing="medium">
-        <Box palette="alert" variation="borders" style="height:3rem;" />
-        <Box palette="affirmative" variation="borders" style="height:3rem;" />
-        <Box palette="negative" variation="borders" style="height:3rem;" />
+        <Box palette="alert" variation="borders" height="icon-massive" />
+        <Box palette="affirmative" variation="borders" height="icon-massive" />
+        <Box palette="negative" variation="borders" height="icon-massive" />
 
-        <Box palette="affirmative" variation="borders" style="height:3rem;" />
-        <Box palette="negative" variation="borders" style="height:3rem;" />
-        <Box palette="alert" variation="borders" style="height:3rem;" />
+        <Box palette="affirmative" variation="borders" height="icon-massive" />
+        <Box palette="negative" variation="borders" height="icon-massive" />
+        <Box palette="alert" variation="borders" height="icon-massive" />
 
-        <Box palette="alert" variation="borders" style="height:3rem;" />
-        <Box palette="affirmative" variation="borders" style="height:3rem;" />
-        <Box palette="negative" variation="borders" style="height:3rem;" />
+        <Box palette="alert" variation="borders" height="icon-massive" />
+        <Box palette="affirmative" variation="borders" height="icon-massive" />
+        <Box palette="negative" variation="borders" height="icon-massive" />
 
-        <Box palette="negative" variation="borders" style="height:3rem;" />
-        <Box palette="alert" variation="borders" style="height:3rem;" />
-        <Box palette="affirmative" variation="borders" style="height:3rem;" />
+        <Box palette="negative" variation="borders" height="icon-massive" />
+        <Box palette="alert" variation="borders" height="icon-massive" />
+        <Box palette="affirmative" variation="borders" height="icon-massive" />
 
-        <Box palette="affirmative" variation="borders" style="height:3rem;" />
-        <Box palette="negative" variation="borders" style="height:3rem;" />
-        <Box palette="alert" variation="borders" style="height:3rem;" />
+        <Box palette="affirmative" variation="borders" height="icon-massive" />
+        <Box palette="negative" variation="borders" height="icon-massive" />
+        <Box palette="alert" variation="borders" height="icon-massive" />
 
-        <Box palette="negative" variation="borders" style="height:3rem;" />
-        <Box palette="alert" variation="borders" style="height:3rem;" />
-        <Box palette="affirmative" variation="borders" style="height:3rem;" />
+        <Box palette="negative" variation="borders" height="icon-massive" />
+        <Box palette="alert" variation="borders" height="icon-massive" />
+        <Box palette="affirmative" variation="borders" height="icon-massive" />
 
-        <Box palette="alert" variation="borders" style="height:3rem;" />
-        <Box palette="affirmative" variation="borders" style="height:3rem;" />
-        <Box palette="negative" variation="borders" style="height:3rem;" />
+        <Box palette="alert" variation="borders" height="icon-massive" />
+        <Box palette="affirmative" variation="borders" height="icon-massive" />
+        <Box palette="negative" variation="borders" height="icon-massive" />
     </Grid.Container>
 </Story>
 
@@ -64,22 +64,26 @@
                 <Text is="strong">{12 - index}</Text>
 
                 <Box palette="inverse" padding="small">
-                    <Grid.Container points={12 - index} spacing="medium" style="width:18rem;">
-                        <Box palette="alert" variation="borders" style="height:3rem;" />
-                        <Box palette="affirmative" variation="borders" style="height:3rem;" />
-                        <Box palette="negative" variation="borders" style="height:3rem;" />
+                    <Grid.Container
+                        points={12 - index}
+                        spacing="medium"
+                        style="width:calc(var(--sizes-icon-massive) * 6);"
+                    >
+                        <Box palette="alert" variation="borders" height="icon-massive" />
+                        <Box palette="affirmative" variation="borders" height="icon-massive" />
+                        <Box palette="negative" variation="borders" height="icon-massive" />
 
-                        <Box palette="affirmative" variation="borders" style="height:3rem;" />
-                        <Box palette="negative" variation="borders" style="height:3rem;" />
-                        <Box palette="alert" variation="borders" style="height:3rem;" />
+                        <Box palette="affirmative" variation="borders" height="icon-massive" />
+                        <Box palette="negative" variation="borders" height="icon-massive" />
+                        <Box palette="alert" variation="borders" height="icon-massive" />
 
-                        <Box palette="affirmative" variation="borders" style="height:3rem;" />
-                        <Box palette="negative" variation="borders" style="height:3rem;" />
-                        <Box palette="alert" variation="borders" style="height:3rem;" />
+                        <Box palette="affirmative" variation="borders" height="icon-massive" />
+                        <Box palette="negative" variation="borders" height="icon-massive" />
+                        <Box palette="alert" variation="borders" height="icon-massive" />
 
-                        <Box palette="affirmative" variation="borders" style="height:3rem;" />
-                        <Box palette="negative" variation="borders" style="height:3rem;" />
-                        <Box palette="alert" variation="borders" style="height:3rem;" />
+                        <Box palette="affirmative" variation="borders" height="icon-massive" />
+                        <Box palette="negative" variation="borders" height="icon-massive" />
+                        <Box palette="alert" variation="borders" height="icon-massive" />
                     </Grid.Container>
                 </Box>
             </div>
@@ -101,13 +105,13 @@
                         spacing={is_default ? undefined : spacing}
                         style="width:20rem;"
                     >
-                        <Box palette="alert" variation="borders" style="height:3rem;" />
-                        <Box palette="affirmative" variation="borders" style="height:3rem;" />
-                        <Box palette="negative" variation="borders" style="height:3rem;" />
+                        <Box palette="alert" variation="borders" height="icon-massive" />
+                        <Box palette="affirmative" variation="borders" height="icon-massive" />
+                        <Box palette="negative" variation="borders" height="icon-massive" />
 
-                        <Box palette="affirmative" variation="borders" style="height:3rem;" />
-                        <Box palette="negative" variation="borders" style="height:3rem;" />
-                        <Box palette="alert" variation="borders" style="height:3rem;" />
+                        <Box palette="affirmative" variation="borders" height="icon-massive" />
+                        <Box palette="negative" variation="borders" height="icon-massive" />
+                        <Box palette="alert" variation="borders" height="icon-massive" />
                     </Grid.Container>
                 </Box>
             </div>
@@ -117,36 +121,36 @@
 
 <Story name="Span">
     <Grid.Container points={["6", "mobile:3", "tablet:4", "desktop:5"]} spacing="medium">
-        <Box palette="alert" variation="borders" style="height:3rem;" />
+        <Box palette="alert" variation="borders" height="icon-massive" />
         <Grid.Item span_x={["4", "mobile:1", "tablet:2", "desktop:3"]} span_y="1">
             <Box palette="affirmative" variation="borders" />
         </Grid.Item>
-        <Box palette="negative" variation="borders" style="height:3rem;" />
+        <Box palette="negative" variation="borders" height="icon-massive" />
 
-        <Box palette="affirmative" variation="borders" style="height:3rem;" />
-        <Box palette="negative" variation="borders" style="height:3rem;" />
-        <Box palette="alert" variation="borders" style="height:3rem;" />
+        <Box palette="affirmative" variation="borders" height="icon-massive" />
+        <Box palette="negative" variation="borders" height="icon-massive" />
+        <Box palette="alert" variation="borders" height="icon-massive" />
 
-        <Box palette="alert" variation="borders" style="height:3rem;" />
-        <Box palette="affirmative" variation="borders" style="height:3rem;" />
-        <Box palette="negative" variation="borders" style="height:3rem;" />
+        <Box palette="alert" variation="borders" height="icon-massive" />
+        <Box palette="affirmative" variation="borders" height="icon-massive" />
+        <Box palette="negative" variation="borders" height="icon-massive" />
 
         <Grid.Item span_x="3" span_y="2">
             <Box palette="negative" variation="borders" />
         </Grid.Item>
-        <Box palette="alert" variation="borders" style="height:3rem;" />
-        <Box palette="affirmative" variation="borders" style="height:3rem;" />
+        <Box palette="alert" variation="borders" height="icon-massive" />
+        <Box palette="affirmative" variation="borders" height="icon-massive" />
 
-        <Box palette="affirmative" variation="borders" style="height:3rem;" />
-        <Box palette="negative" variation="borders" style="height:3rem;" />
-        <Box palette="alert" variation="borders" style="height:3rem;" />
+        <Box palette="affirmative" variation="borders" height="icon-massive" />
+        <Box palette="negative" variation="borders" height="icon-massive" />
+        <Box palette="alert" variation="borders" height="icon-massive" />
 
-        <Box palette="negative" variation="borders" style="height:3rem;" />
-        <Box palette="alert" variation="borders" style="height:3rem;" />
-        <Box palette="affirmative" variation="borders" style="height:3rem;" />
+        <Box palette="negative" variation="borders" height="icon-massive" />
+        <Box palette="alert" variation="borders" height="icon-massive" />
+        <Box palette="affirmative" variation="borders" height="icon-massive" />
 
-        <Box palette="alert" variation="borders" style="height:3rem;" />
-        <Box palette="affirmative" variation="borders" style="height:3rem;" />
-        <Box palette="negative" variation="borders" style="height:3rem;" />
+        <Box palette="alert" variation="borders" height="icon-massive" />
+        <Box palette="affirmative" variation="borders" height="icon-massive" />
+        <Box palette="negative" variation="borders" height="icon-massive" />
     </Grid.Container>
 </Story>

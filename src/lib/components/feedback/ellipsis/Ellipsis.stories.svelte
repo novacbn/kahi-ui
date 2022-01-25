4b43c94a80d10be6ede9069171e00ef705a0dcb5
novacbn <31122674+novacbn@users.noreply.github.com>
2022-01-25 15:30:40 -0500

    - Updated `Ellipsis` to utilize `Animation` with calculated `<Animation delay>` - Added `<Ellipsis iterations={number}>` support for controlling how many "ellipsis items" are rendered - Added `<Ellipsis animation="bounce/pulse/ping">` for configuring inner `Animation` Components

--- src/lib/components/feedback/ellipsis/Ellipsis.stories.svelte
+++ src/lib/components/feedback/ellipsis/Ellipsis.stories.svelte
@@ -2,11 +2,14 @@
     import {Meta, Story, Template} from "@storybook/addon-svelte-csf";
 
     import Button from "../../interactables/button/Button.svelte";
+    import NumberInput from "../../interactables/numberinput/NumberInput.svelte";
     import * as Stack from "../../layouts/stack";
     import Heading from "../../typography/heading/Heading.svelte";
     import Text from "../../typography/text/Text.svelte";
 
     import Ellipsis from "./Ellipsis.svelte";
+
+    let iterations = 3;
 </script>
 
 <Meta title="Feedback/Ellipsis" />
@@ -31,9 +34,41 @@
     </Stack.Container>
 </Story>
 
+<Story name="Iterations">
+    <NumberInput bind:value={iterations} />
+
+    <Heading>
+        Loading
+        {#key iterations}
+            <Ellipsis {iterations} />
+        {/key}
+    </Heading>
+</Story>
+
 <Story name="Slot">
     <Heading>
         Loading
         <Ellipsis>/</Ellipsis>
+    </Heading>
+</Story>
+
+<Story name="Bounce">
+    <Heading>
+        Loading
+        <Ellipsis animation="bounce" />
+    </Heading>
+</Story>
+
+<Story name="Pulse">
+    <Heading>
+        Loading
+        <Ellipsis animation="pulse" />
+    </Heading>
+</Story>
+
+<Story name="Ping">
+    <Heading>
+        Loading
+        <Ellipsis animation="ping" />
     </Heading>
 </Story>

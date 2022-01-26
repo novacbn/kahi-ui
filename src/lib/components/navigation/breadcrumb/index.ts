665acac8c9414b441f606badb0dc17857f0757f3
novacbn <31122674+novacbn@users.noreply.github.com>
2022-01-26 11:55:04 -0500

    - Removed outline from "Anchor"-based Components - Added `<Breadcrumb.Button>` exports

--- src/lib/components/navigation/breadcrumb/index.ts
+++ src/lib/components/navigation/breadcrumb/index.ts
@@ -1,5 +1,6 @@
 export {default as Container} from "./BreadcrumbContainer.svelte";
 
 export {default as Anchor} from "./BreadcrumbAnchor.svelte";
+export {default as Button} from "./BreadcrumbButton.svelte";
 export {default as Group} from "./BreadcrumbGroup.svelte";
 export {default as Separator} from "./BreadcrumbSeparator.svelte";

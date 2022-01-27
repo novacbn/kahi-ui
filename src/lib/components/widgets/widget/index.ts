296cd7647c377fe8a37a65a8e861f911afe91810
novacbn <31122674+novacbn@users.noreply.github.com>
2022-01-27 17:16:03 -0500

    - Remove obsoleted code

--- src/lib/components/widgets/widget/index.ts
+++ src/lib/components/widgets/widget/index.ts
@@ -1,5 +1,4 @@
 export {default as Container} from "./WidgetContainer.svelte";
 
 export {default as Button} from "./WidgetButton.svelte";
-export {default as Header} from "./WidgetHeader.svelte";
 export {default as Section} from "./WidgetSection.svelte";
--- src/lib/util/datetime.ts
+++ src/lib/util/datetime.ts
@@ -3,42 +3,9 @@ import {Temporal} from "../vendor/js-temporal-polyfill";
 import type {PROPERTY_CLOCK_PERIOD} from "../types/datetime";
 import {TOKENS_CLOCK_PERIOD} from "../types/datetime";
 
-import {chunk, fill} from "./functional";
-import {DEFAULT_CALENDAR} from "./locale";
-import {wrap} from "./math";
+import {fill} from "./functional";
 
 const EXPRESSION_TIMEZONE = /\[[\w/]+\]$/;
-
-function get_calendar_day(date: Temporal.PlainDate): number {
-    return wrap(date.dayOfWeek + 1, 1, date.daysInWeek);
-}
-
-function to_plain_year(year: string | Temporal.PlainYearMonthLike): Temporal.PlainYearMonth {
-    // NOTE: We need to set the `Temporal.PlainYearMonth.month` values to
-    // `1` (January) to bypass month checking, sticking to only year / calendar
-
-    return Temporal.PlainYearMonth.from(year).with({month: 1});
-}
-
-export function get_calendar_weeks(
-    month: string | Temporal.PlainYearMonthLike
-): Temporal.PlainDate[][] {
-    const _month = Temporal.PlainYearMonth.from(month).toPlainDate({day: 1});
-
-    const starting_day = _month.subtract({
-        days: get_calendar_day(_month) - 1,
-    });
-
-    let ending_day = _month.with({day: _month.daysInMonth});
-    ending_day = ending_day.add({days: ending_day.daysInWeek - get_calendar_day(ending_day) + 1});
-
-    const duration = starting_day.until(ending_day);
-
-    return chunk(
-        fill((index) => starting_day.add({days: index}), duration.total({unit: "day"})),
-        _month.daysInWeek
-    );
-}
 
 export function get_clock_ranges(
     value?: string | Temporal.PlainTimeLike,
@@ -66,42 +33,13 @@ export function get_clock_ranges(
     ];
 }
 
-export function get_daystamp(calendar: string = DEFAULT_CALENDAR): string {
-    return Temporal.Now.plainDate(calendar).toString({calendarName: "always"});
-}
-
-export function get_monthstamp(calendar: string = DEFAULT_CALENDAR): string {
-    return Temporal.Now.plainDate(calendar).toPlainYearMonth().toString({calendarName: "always"});
-}
-
 export function get_timestamp(): string {
     // NOTE: Temporal API only accepts ISO 8601 for zoned time
     return Temporal.Now.zonedDateTimeISO().toString({calendarName: "always"});
 }
 
-export function has_day(
-    days: readonly (string | Temporal.PlainDateLike)[],
-    day: string | Temporal.PlainDateLike
-): boolean {
-    const _day = Temporal.PlainDate.from(day);
-
-    return !!days.find((entry) => _day.equals(entry));
-}
-
 export function has_timezone(timestamp: string): boolean {
     return EXPRESSION_TIMEZONE.test(timestamp);
-}
-
-export function is_day_in_range(
-    month: string | Temporal.PlainDateLike,
-    max?: string | Temporal.PlainDateLike,
-    min?: string | Temporal.PlainDateLike,
-    inclusive: boolean = false
-): boolean {
-    if (max && Temporal.PlainDate.compare(max, month) < (inclusive ? 0 : 1)) return false;
-    else if (min && Temporal.PlainDate.compare(min, month) > (inclusive ? 0 : -1)) return false;
-
-    return true;
 }
 
 export function is_time_in_range(

309638107f9286b3c64022b7ed420fa84b765073
novacbn <31122674+novacbn@users.noreply.github.com>
2022-02-07 18:51:55 -0500

    - Migrated hotpath string functionality from SASS to Javascript - Updated stylesheet contexts to use internal local cache, didn't make sense to use namespaces anyway
        - Getting to the tail end of the stylesheet rework and build times are in the 23~28s range for me (Ryzen 3550H) slowing down iteration. With these changes, it's now 8~9s, which is a lot more manageable. And in watch mode it's slightly faster.
    
        - Ultimately in a future `>= 0.6.X` release, the namespace handling will completely done in JS-land anyway. I would imagine it would bring a slight bump in speed there too. But doing so will facilitate dumping the variable registry into a JSON file and having a script generate typings for JS-land theme customization.
        - e.g. `<Card.Footer --card-padding-x="0.2">`

--- scripts/functions.js
+++ scripts/functions.js
@@ -2,7 +2,9 @@ import sass from "sass";
 
 import {BUILD_FLAGS} from "./flags.js";
 
-const EXPRESSION_DEQUOTE = /^['"]?([\w_]*)['"]?$/;
+const EXPRESSION_DEQUOTE = /^['"]?([\w_\.\-\%\*]*)['"]?$/;
+
+const EXPRESSION_FILTER_SAFE = /([\^\$\?\.\*\-\+\*\[\]])/g;
 
 const SASS_ENVIRONMENT = {
     ...BUILD_FLAGS,
@@ -10,8 +12,13 @@ const SASS_ENVIRONMENT = {
 
 function get_string(value, name) {
     const string = value.assertString(name).toString();
+    const match = EXPRESSION_DEQUOTE.exec(string);
 
-    return EXPRESSION_DEQUOTE.exec(string)[1];
+    return match ? match[1] : string;
+}
+
+function to_regexp_safe(str) {
+    return str.replace(EXPRESSION_FILTER_SAFE, (substr) => `\\${substr}`);
 }
 
 // NOTE: Most if not all SASS functions created for the Framework SHOULD
@@ -39,5 +46,50 @@ export const SASS_FUNCTIONS = {
             default:
                 return new sass.SassString(value.toString());
         }
+    },
+
+    "str-endswith($str, $search)": (args) => {
+        const str = get_string(args[0], "str");
+        const search = get_string(args[1], "search");
+
+        return str.endsWith(search) ? new sass.SassNumber(-search.length) : sass.sassNull;
+    },
+
+    "str-filter($str, $search: null)": (args) => {
+        if (args[1] === sass.sassNull) return args[0];
+
+        const str = get_string(args[0], "str");
+        const search = get_string(args[1], "search");
+
+        if (search.includes("*")) {
+            let [first_substr, last_substr] = search.split("*");
+
+            first_substr = to_regexp_safe(first_substr);
+            last_substr = to_regexp_safe(last_substr);
+
+            const expression = new RegExp(`^${first_substr}(.*)${last_substr}$`);
+            const match = expression.exec(str);
+
+            if (match) return new sass.SassString(match[1]);
+        } else {
+            if (str.startsWith(search)) return new sass.SassString(str.slice(search.length));
+        }
+
+        return sass.sassNull;
+    },
+
+    "str-replaceall($str, $search, $replace)": (args) => {
+        const str = get_string(args[0], "str");
+        const search = get_string(args[1], "search");
+        const replace = get_string(args[2], "replace");
+
+        return new sass.SassString(str.replaceAll(search, replace));
+    },
+
+    "str-startswith($str, $search)": (args) => {
+        const str = get_string(args[0], "str");
+        const search = get_string(args[1], "search");
+
+        return str.startsWith(search) ? new sass.SassNumber(search.length) : sass.sassNull;
     },
 };

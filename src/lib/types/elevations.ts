722f9dd0dcdbe279908f06e81424230ba2c8eec0
novacbn <31122674+novacbn@users.noreply.github.com>
2022-02-14 17:17:09 -0500

    - Fixed `<* elevation>` typings to include new elevation tiers

--- src/lib/types/elevations.ts
+++ src/lib/types/elevations.ts
@@ -1,4 +1,5 @@
 import type {LiteralEnum} from "./util";
+import type {BreakpointEnum} from "./viewports";
 
 /**
  * Represents the tiers of elevation (box-shadow) tokens that can be applied to Framework Components
@@ -6,13 +7,19 @@ import type {LiteralEnum} from "./util";
 export enum TOKENS_ELEVATION {
     lowest = "lowest",
 
+    lower = "lower",
+
     low = "low",
 
     medium = "medium",
 
     high = "high",
 
+    higher = "higher",
+
     highest = "highest",
 }
 
 export type PROPERTY_ELEVATION = LiteralEnum<TOKENS_ELEVATION>;
+
+export type PROPERTY_ELEVATION_BREAKPOINT = BreakpointEnum<PROPERTY_ELEVATION>;

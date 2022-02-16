ca9e5baf6b1b305a444e3fb7ad5bb72b4e96c496
novacbn <31122674+novacbn@users.noreply.github.com>
2022-02-15 21:24:15 -0500

    - Added "Elements" story for `Breadcrumb` to demo `<button>` tags - Updated "Palette" story for `Breadcrumb` to render palette names

--- src/lib/components/navigation/breadcrumb/Breadcrumb.stories.svelte
+++ src/lib/components/navigation/breadcrumb/Breadcrumb.stories.svelte
@@ -47,8 +47,27 @@
             <Breadcrumb.Container palette={is_default ? undefined : palette}>
                 <Breadcrumb.Anchor href="#">Home</Breadcrumb.Anchor>
                 <Breadcrumb.Anchor href="#">Contacts</Breadcrumb.Anchor>
-                <Breadcrumb.Anchor href="#" active>John Doe</Breadcrumb.Anchor>
+
+                <Breadcrumb.Anchor href="#" active>
+                    {`${palette.toUpperCase()}${is_default ? " / DEFAULT" : ""}`}
+                </Breadcrumb.Anchor>
             </Breadcrumb.Container>
         {/each}
+    </Stack.Container>
+</Story>
+
+<Story name="Elements">
+    <Stack.Container orientation="horizontal" spacing="medium" variation="wrap">
+        <Breadcrumb.Container>
+            <Breadcrumb.Anchor href="#">Home</Breadcrumb.Anchor>
+            <Breadcrumb.Anchor href="#">Contacts</Breadcrumb.Anchor>
+            <Breadcrumb.Anchor href="#" active>Anchor</Breadcrumb.Anchor>
+        </Breadcrumb.Container>
+
+        <Breadcrumb.Container>
+            <Breadcrumb.Button>Home</Breadcrumb.Button>
+            <Breadcrumb.Button>Contacts</Breadcrumb.Button>
+            <Breadcrumb.Button active>Button</Breadcrumb.Button>
+        </Breadcrumb.Container>
     </Stack.Container>
 </Story>

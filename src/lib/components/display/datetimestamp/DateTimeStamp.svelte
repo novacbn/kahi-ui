c2a38104e43597be13d12e58139d8f4fe9fb9c6e
novacbn <31122674+novacbn@users.noreply.github.com>
2022-03-02 03:29:24 -0500

    - Updated `<*Stamp>` Components to utilize internal helpers

--- src/lib/components/display/datetimestamp/DateTimeStamp.svelte
+++ src/lib/components/display/datetimestamp/DateTimeStamp.svelte
@@ -1,6 +1,4 @@
 <script lang="ts">
-    import {Temporal} from "../../../vendor/js-temporal-polyfill";
-
     import type {IGlobalProperties} from "../../../types/global";
     import type {IHTML5Events, IHTML5Properties} from "../../../types/html5";
     import type {IMarginProperties} from "../../../types/spacings";
@@ -10,12 +8,12 @@
 
     import {map_global_attributes} from "../../../util/attributes";
     import type {IDayFormatOptions} from "../../../util/datetime/days";
+    import {from_datetimestamp, to_datetimestamp} from "../../../util/datetime/datetimestamps";
     import type {IHourFormatOptions} from "../../../util/datetime/hours";
     import type {IMinuteFormatOptions} from "../../../util/datetime/minutes";
     import type {IMonthFormatOptions} from "../../../util/datetime/months";
     import type {IYearFormatOptions} from "../../../util/datetime/years";
     import type {ISecondFormatOptions} from "../../../util/datetime/seconds";
-    import {has_timezone} from "../../../util/datetime/timezones";
     import {defaultopt} from "../../../util/functional";
     import {DEFAULT_CALENDAR, DEFAULT_FORMAT_DATETIME, DEFAULT_LOCALE} from "../../../util/locale";
 
@@ -67,9 +65,7 @@
     // TODO: Can you convert between calendars? If so, should always convert
     // to a Gregorian Calendar since `<time>` only supports that
 
-    $: _datetime = has_timezone(timestamp)
-        ? Temporal.ZonedDateTime.from(timestamp)
-        : Temporal.PlainDateTime.from(timestamp);
+    $: _datetime = from_datetimestamp(timestamp);
     $: _options = defaultopt(
         {day, month, weekday, year, hour, hour_12, minute, second},
         DEFAULT_FORMAT_DATETIME
@@ -80,7 +76,7 @@
     bind:this={element}
     {...map_global_attributes($$restProps)}
     class="date-time-stamp {_class}"
-    datetime={_datetime.withCalendar("iso8601").toString({calendarName: "never"})}
+    datetime={to_datetimestamp(_datetime)}
     use:forward_actions={{actions}}
     on:click
     on:contextmenu
---
+++ src/lib/util/datetime/datetimestamps.ts
@@ -0,0 +1,44 @@
+import {Temporal} from "../../vendor/js-temporal-polyfill";
+
+const EXPRESSION_INSTANT = /Z$/;
+
+const EXPRESSION_OFFSET = /[\+\-]\d\d:\d\d$/;
+
+const EXPRESSION_TIMEZONE = /\[[\w/]+\]$/;
+
+export function from_datetimestamp(
+    datetimestamp: string,
+    modify?: Temporal.PlainDateTimeLike
+): Temporal.PlainDateTime | Temporal.Instant | Temporal.ZonedDateTime {
+    if (EXPRESSION_OFFSET.test(datetimestamp) || EXPRESSION_INSTANT.test(datetimestamp)) {
+        return Temporal.Instant.from(datetimestamp);
+    } else if (EXPRESSION_TIMEZONE.test(datetimestamp)) {
+        return modify
+            ? Temporal.ZonedDateTime.from(datetimestamp).with(modify)
+            : Temporal.ZonedDateTime.from(datetimestamp);
+    }
+
+    return modify
+        ? Temporal.PlainDateTime.from(datetimestamp).with(modify)
+        : Temporal.PlainDateTime.from(datetimestamp);
+}
+
+export function to_datetimestamp(
+    datetimestamp: Temporal.PlainDateTime | Temporal.Instant | Temporal.ZonedDateTime,
+    modify?: Temporal.PlainDateTimeLike
+): string {
+    if (datetimestamp instanceof Temporal.Instant) return datetimestamp.toString();
+    else if (datetimestamp instanceof Temporal.ZonedDateTime)
+        return modify
+            ? datetimestamp
+                  .withCalendar("iso8601")
+                  .with(modify)
+                  .toString({calendarName: "never", timeZoneName: "never"})
+            : datetimestamp
+                  .withCalendar("iso8601")
+                  .toString({calendarName: "never", timeZoneName: "never"});
+
+    return modify
+        ? datetimestamp.withCalendar("iso8601").with(modify).toString({calendarName: "never"})
+        : datetimestamp.withCalendar("iso8601").toString({calendarName: "never"});
+}

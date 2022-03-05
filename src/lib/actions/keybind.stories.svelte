5d33d2f6fabb2022630a5d031256137d570add91
novacbn <31122674+novacbn@users.noreply.github.com>
2022-03-05 03:12:52 -0500

    - Added story for testing built-in shortcuts

--- src/lib/actions/keybind.stories.svelte
+++ src/lib/actions/keybind.stories.svelte
@@ -1,7 +1,30 @@
 <script>
     import {Meta, Story, Template} from "@storybook/addon-svelte-csf";
+    import Text from "../components/typography/text/Text.svelte";
 
-    import {keybind} from "./keybind";
+    import {
+        action_activate,
+        action_exit,
+        action_submit,
+        keybind,
+        navigate_down,
+        navigate_left,
+        navigate_right,
+        navigate_up,
+    } from "./keybind";
+
+    const BUILTIN_SHORTCUTS = [
+        [action_activate, "activate", ["ENTER", "SPACE"]],
+        [action_exit, "exit", ["ESCAPE"]],
+        [action_submit, "submit", ["ENTER"]],
+        [navigate_down, "down", ["DOWNARROW"]],
+        [navigate_left, "left", ["LEFTARROW"]],
+        [navigate_right, "right", ["RIGHTARROW"]],
+        [navigate_up, "up", ["UPARROW"]],
+    ];
+
+    let is_active = false;
+    let shortcuts = {};
 
     function on_bind(event) {
         event.preventDefault();
@@ -9,9 +32,11 @@
         is_active = event.detail.active;
     }
 
-    let element;
+    function on_shortcut(event, name) {
+        event.preventDefault();
 
-    export let is_active = false;
+        shortcuts = {...shortcuts, [name]: event.detail.active};
+    }
 </script>
 
 <svelte:window
@@ -41,12 +66,27 @@
 </Story>
 
 <Story name="svelte:window">
-    <div
-        bind:this={element}
-        class="box"
-        data-palette={is_active ? "affirmative" : "negative"}
-        data-padding="small"
-    >
+    <div class="box" data-palette={is_active ? "affirmative" : "negative"} data-padding="small">
         Press CTRL+/ to activate bind.
     </div>
+</Story>
+
+<Story name="Built-in Shortcuts">
+    {#each BUILTIN_SHORTCUTS as [shortcut, name, binds] (name)}
+        <div>
+            <Text is="strong">{name}</Text>
+            <br />
+            <input
+                class="text-input"
+                type="text"
+                placeholder="Click inside of me for focus, and press {binds.join(
+                    ', '
+                )} to activate bind."
+                data-palette={shortcuts[name] ? "affirmative" : "negative"}
+                use:shortcut={{
+                    on_bind: (event) => on_shortcut(event, name),
+                }}
+            />
+        </div>
+    {/each}
 </Story>
